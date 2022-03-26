9070f52d8f608020bd5c7b64fbe46b77531a73d8
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-26 14:44:53 +0800

    >  sim RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:44:53 up  4:41,  5 users,  load average: 0.10, 0.08, 0.02

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-STUID = ysyx_22040000
-STUNAME = 张三
+STUID = ysyx_22040495
+STUNAME = 陈昇杰 
 
 # DO NOT modify the following code!!!
 
---
+++ npc/csrc/sim.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Vtop.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+vluint64_t main_time = 0;
+const vluint64_t sim_time = 1024;
+
+int main(int argc, char **argv, char **env){
+	Verilated::commandArgs(argc, argv);
+	Verilated::traceEverOn(true);
+
+	Vtop *top = new Vtop;
+	VerilatedVcdC *tfp = new VerilatedVcdC;
+
+	top->trace(tfp,99);
+	tfp->open("Top.vcd");
+	
+	while (!Verilated::gotFinish() && main_time < sim_time) {
+		int a = rand() & 1;
+		int b = rand() & 1;
+		top->a = a;
+		top->b = b;
+
+		top->eval();
+		tfp->dump(main_time);
+
+		main_time++;
+		printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+		assert(top->f == a ^ b);
+	}
+	tfp->close();
+	delete top;
+	delete tfp;
+	exit(0);
+	return 0;
+}
