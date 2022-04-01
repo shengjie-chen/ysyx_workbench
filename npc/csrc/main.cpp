292af2e2eb75fc7e958dae27e2a82341de129c13
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-01 22:24:37 +0800

    >  sim RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:24:37 up 39 min,  4 users,  load average: 0.00, 0.02, 0.03

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -6,20 +6,10 @@ static TOP_NAME dut;
 
 void nvboard_bind_all_pins(Vtop* top);
 
-static void single_cysle() {
-		dut.clk = 0; dut.eval();
-		dut.clk = 1; dut.eval();
-}
-
-static void reset(int n) {
-		dut.rst = 1;
-		while (n -- > 0) single_cycle();
-		dut.rst = 0;
-}
 
 int main() {
 	nvboard_bind_all_pins(&dut);
-	nvboaed_init();
+	nvboard_init();
 
 	while(1){
 		nvboard_update();
