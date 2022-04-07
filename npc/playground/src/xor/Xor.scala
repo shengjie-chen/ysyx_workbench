1faa6b39bc7c12122caaa550485f8dc9d3b0e9c8
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-07 22:59:31 +0800

    >  generate verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:59:31 up 41 min,  4 users,  load average: 0.11, 0.10, 0.15

---
+++ npc/playground/src/xor/Xor.scala
@@ -0,0 +1,10 @@
+import chisel3._
+
+class Xor extends Module {
+  val io = IO(new Bundle {
+    val a = Input(UInt(1.W))
+    val b = Input(UInt(1.W))
+    val f = Output(UInt(1.W))
+  }
+  io.f := io.a ^ io.b
+}
