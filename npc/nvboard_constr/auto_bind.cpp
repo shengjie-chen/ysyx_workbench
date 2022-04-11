7e3711139f6b85b08a325acdc729dc525633572d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-12 01:56:33 +0800

    >  sim RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:56:33 up 10:02,  1 user,  load average: 0.84, 0.86, 0.63

--- npc/nvboard_constr/auto_bind.cpp
+++ npc/nvboard_constr/auto_bind.cpp
@@ -1,8 +1,11 @@
 #include <nvboard.h>
-#include "VXor.h"
+#include "VMultiplexer.h"
 
-void nvboard_bind_all_pins(VXor* top) {
-	nvboard_bind_pin( &top->io_f, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD0);
-	nvboard_bind_pin( &top->io_a, BIND_RATE_SCR, BIND_DIR_IN , 1, SW0);
-	nvboard_bind_pin( &top->io_b, BIND_RATE_SCR, BIND_DIR_IN , 1, SW1);
+void nvboard_bind_all_pins(VMultiplexer* top) {
+	nvboard_bind_pin( &top->io_X_0, BIND_RATE_SCR, BIND_DIR_IN , 2, SW3, SW2);
+	nvboard_bind_pin( &top->io_X_1, BIND_RATE_SCR, BIND_DIR_IN , 2, SW5, SW4);
+	nvboard_bind_pin( &top->io_X_2, BIND_RATE_SCR, BIND_DIR_IN , 2, SW7, SW6);
+	nvboard_bind_pin( &top->io_X_3, BIND_RATE_SCR, BIND_DIR_IN , 2, SW9, SW8);
+	nvboard_bind_pin( &top->io_Y, BIND_RATE_SCR, BIND_DIR_IN , 2, SW1, SW0);
+	nvboard_bind_pin( &top->io_F, BIND_RATE_SCR, BIND_DIR_OUT, 2, LD1, LD0);
 }
