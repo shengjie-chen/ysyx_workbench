12c5c79360cb7a78b56ddbe749095ac724f05078
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-12 01:52:07 +0800

    >  sim RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:52:07 up  9:58,  1 user,  load average: 1.37, 0.78, 0.54

---
+++ npc/nvboard_constr/auto_bind.cpp
@@ -0,0 +1,8 @@
+#include <nvboard.h>
+#include "VXor.h"
+
+void nvboard_bind_all_pins(VXor* top) {
+	nvboard_bind_pin( &top->io_f, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD0);
+	nvboard_bind_pin( &top->io_a, BIND_RATE_SCR, BIND_DIR_IN , 1, SW0);
+	nvboard_bind_pin( &top->io_b, BIND_RATE_SCR, BIND_DIR_IN , 1, SW2);
+}
