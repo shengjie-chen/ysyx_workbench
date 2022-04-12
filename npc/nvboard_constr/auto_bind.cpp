6e4604902cf4c39f9cedee32c1bc013ae95ec48b
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-12 23:22:27 +0800

    >  sim RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:22:27 up 10:53,  1 user,  load average: 0.20, 0.19, 0.60

--- npc/nvboard_constr/auto_bind.cpp
+++ npc/nvboard_constr/auto_bind.cpp
@@ -5,8 +5,8 @@ void nvboard_bind_all_pins(VAluFourBit* top) {
 	nvboard_bind_pin( &top->io_fun, BIND_RATE_SCR, BIND_DIR_IN , 3, SW10, SW9, SW8);
 	nvboard_bind_pin( &top->io_A, BIND_RATE_SCR, BIND_DIR_IN , 4, SW3, SW2, SW1, SW0);
 	nvboard_bind_pin( &top->io_B, BIND_RATE_SCR, BIND_DIR_IN , 4, SW7, SW6, SW5, SW4);
-	nvboard_bind_pin( &top->io_out, BIND_RATE_SCR, BIND_DIR_OUT, 3, LD2, LD1, LD0);
-	nvboard_bind_pin( &top->io_zero, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD3);
-	nvboard_bind_pin( &top->io_overflow, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD4);
-	nvboard_bind_pin( &top->io_carry, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD5);
+	nvboard_bind_pin( &top->io_out, BIND_RATE_SCR, BIND_DIR_OUT, 4, LD3, LD2, LD1, LD0);
+	nvboard_bind_pin( &top->io_zero, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD4);
+	nvboard_bind_pin( &top->io_overflow, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD5);
+	nvboard_bind_pin( &top->io_carry, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD6);
 }
