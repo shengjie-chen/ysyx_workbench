c117efaccecff31bdec685d1b8401e42580109c0
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-12 14:14:53 +0800

    >  generate verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:14:53 up  1:45,  1 user,  load average: 0.50, 0.35, 0.30

---
+++ npc/playground/src/circuit_exp/PriorEncoder/PriorEncoder_sim.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "time.h"
+#include "VPriorEncoder.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+vluint64_t main_time = 0;
+const vluint64_t sim_time = 256;
+
+int main(int argc, char **argv, char **env){
+	Verilated::commandArgs(argc, argv);
+	Verilated::traceEverOn(true);
+
+	VPriorEncoder *top = new VPriorEncoder;
+	VerilatedVcdC *tfp = new VerilatedVcdC;
+
+	top->trace(tfp,99);
+	tfp->open("./build/circuit_exp/PriorEncoder/PriorEncoder.vcd");
+
+    srand(time(NULL));
+	while (!Verilated::gotFinish() && main_time < sim_time) {
+
+		int in[8];
+
+        for(int i=0;i<8:i++){
+            in[i] = rand() % 2
+            top.io_in(0) = in[i]
+        }
+
+		top->eval();
+		tfp->dump(main_time);
+
+		main_time++;
+
+		printf("in = ");
+        for(int i=0;i<8:i++){
+            printf("%d",in[7-i])
+        }
+		printf("\n");
+		printf("io_out = %d\n",top.io_out)
+        printf("io_in_valid = %d\n",top.io_in_valid)
+
+		int out,in_valid = 0;
+		for(int i=0;i<8;i++){
+		    if(in[7-i]){
+		        out = 7-i;
+		        in_valid = 1;
+		        break;
+		    }
+		}
+		assert(top->io_in_valid == in_valid);
+		assert(top->io_out == out);
+	}
+	tfp->close();
+	delete top;
+	delete tfp;
+	exit(0);
+	return 0;
+}
