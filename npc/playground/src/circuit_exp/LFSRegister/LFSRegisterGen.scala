a269843b9d394773230e8e43893f8ebc3ed032fc
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-14 14:53:36 +0800

    >  generate verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:53:36 up  3:31,  1 user,  load average: 0.21, 0.49, 0.49

--- npc/playground/src/circuit_exp/LFSRegister/LFSRegisterGen.scala
+++ npc/playground/src/circuit_exp/LFSRegister/LFSRegisterGen.scala
@@ -1,5 +1,3 @@
-package circuit_exp.LFSRegister
-
 object LFSRegisterGen extends App{
   (new chisel3.stage.ChiselStage).execute(args,
     Seq(chisel3.stage.ChiselGeneratorAnnotation(()=>new LFSRegister())))
