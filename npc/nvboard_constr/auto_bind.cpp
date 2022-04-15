cad954d779ed1973f4bea64e83208281b862c04f
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-15 23:16:40 +0800

    >  sim KeyBoard RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:16:40 up  8:20,  1 user,  load average: 0.06, 0.15, 0.15

--- npc/nvboard_constr/auto_bind.cpp
+++ npc/nvboard_constr/auto_bind.cpp
@@ -1,11 +1,14 @@
 #include <nvboard.h>
-#include "VLFSRegister.h"
+#include "VKeyBoard.h"
 
-void nvboard_bind_all_pins(VLFSRegister* top) {
-	nvboard_bind_pin( &top->clock, BIND_RATE_SCR, BIND_DIR_IN , 1, BTNC);
-	nvboard_bind_pin( &top->io_set, BIND_RATE_SCR, BIND_DIR_IN , 1, SW15);
-	nvboard_bind_pin( &top->io_in, BIND_RATE_SCR, BIND_DIR_IN , 8, SW7, SW6, SW5, SW4, SW3, SW2, SW1, SW0);
-	nvboard_bind_pin( &top->io_out, BIND_RATE_SCR, BIND_DIR_OUT, 8, LD7, LD6, LD5, LD4, LD3, LD2, LD1, LD0);
+void nvboard_bind_all_pins(VKeyBoard* top) {
+	nvboard_bind_pin( &top->io_ps2_clk, BIND_RATE_RT , BIND_DIR_IN , 1, PS2_CLK);
+	nvboard_bind_pin( &top->io_ps2_data, BIND_RATE_RT , BIND_DIR_IN , 1, PS2_DAT);
+	nvboard_bind_pin( &top->io_data, BIND_RATE_SCR, BIND_DIR_OUT, 8, LD7, LD6, LD5, LD4, LD3, LD2, LD1, LD0);
 	nvboard_bind_pin( &top->io_seg0, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG0A, SEG0B, SEG0C, SEG0D, SEG0E, SEG0F, SEG0G, DEC0P);
 	nvboard_bind_pin( &top->io_seg1, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG1A, SEG1B, SEG1C, SEG1D, SEG1E, SEG1F, SEG1G, DEC1P);
+	nvboard_bind_pin( &top->io_seg2, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG2A, SEG2B, SEG2C, SEG2D, SEG2E, SEG2F, SEG2G, DEC2P);
+	nvboard_bind_pin( &top->io_seg3, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG3A, SEG3B, SEG3C, SEG3D, SEG3E, SEG3F, SEG3G, DEC3P);
+	nvboard_bind_pin( &top->io_seg4, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG4A, SEG4B, SEG4C, SEG4D, SEG4E, SEG4F, SEG4G, DEC4P);
+	nvboard_bind_pin( &top->io_seg5, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG5A, SEG5B, SEG5C, SEG5D, SEG5E, SEG5F, SEG5G, DEC5P);
 }
