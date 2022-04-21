6e84b349547ba9a73b2d10169aa0c56a02ef4099
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-21 19:50:54 +0800

    >  sim VgaOutput RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:50:54 up 44 min,  1 user,  load average: 0.48, 0.34, 0.45

--- npc/nvboard_constr/auto_bind.cpp
+++ npc/nvboard_constr/auto_bind.cpp
@@ -2,8 +2,8 @@
 #include "VVgaOutput.h"
 
 void nvboard_bind_all_pins(VVgaOutput* top) {
-	nvboard_bind_pin( &top->io_VGA_VSYNC, BIND_RATE_RT , BIND_DIR_OUT, 1, VGA_VSYNC);
-	nvboard_bind_pin( &top->io_VGA_HSYNC, BIND_RATE_RT , BIND_DIR_OUT, 1, VGA_HSYNC);
+	nvboard_bind_pin( &top->io_VGA_VS, BIND_RATE_RT , BIND_DIR_OUT, 1, VGA_VSYNC);
+	nvboard_bind_pin( &top->io_VGA_HS, BIND_RATE_RT , BIND_DIR_OUT, 1, VGA_HSYNC);
 	nvboard_bind_pin( &top->io_VGA_BLANK_N, BIND_RATE_RT , BIND_DIR_OUT, 1, VGA_BLANK_N);
 	nvboard_bind_pin( &top->io_VGA_R, BIND_RATE_RT , BIND_DIR_OUT, 8, VGA_R7, VGA_R6, VGA_R5, VGA_R4, VGA_R3, VGA_R2, VGA_R1, VGA_R0);
 	nvboard_bind_pin( &top->io_VGA_G, BIND_RATE_RT , BIND_DIR_OUT, 8, VGA_G7, VGA_G6, VGA_G5, VGA_G4, VGA_G3, VGA_G2, VGA_G1, VGA_G0);
