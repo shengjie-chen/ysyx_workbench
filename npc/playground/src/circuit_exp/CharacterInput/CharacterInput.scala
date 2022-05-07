6a6256490452768510a84e90097dc9b3921e3dd2
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-07 23:16:21 +0800

    >  generate CharacterInput verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:16:21 up  2:16,  1 user,  load average: 1.05, 0.82, 0.52

--- npc/playground/src/circuit_exp/CharacterInput/CharacterInput.scala
+++ npc/playground/src/circuit_exp/CharacterInput/CharacterInput.scala
@@ -27,8 +27,8 @@ class CharacterInput extends Module {
   io.VGA_BLANK_N  := vga.io.VGA_BLANK_N
   io.VGA_HS       := vga.io.VGA_HS
   io.VGA_VS       := vga.io.VGA_VS
-  io.ps2_clk      := keyboard.io.ps2_clk
-  io.ps2_data     := keyboard.io.ps2_data
+  keyboard.io.ps2_clk      := io.ps2_clk
+  keyboard.io.ps2_data     := io.ps2_data
   vga.io.mem_write_data   := keyboard.io.data
   vga.io.mem_write_en     := keyboard.io.mem_write_en
 }
