f04ef97e9cf12c1b81566b5fe0347c4e7f8dabfb
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-14 22:36:37 +0800

    >  generate CharacterInput verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:36:37 up 39 min,  1 user,  load average: 0.33, 0.53, 0.59

--- npc/playground/src/circuit_exp/CharacterInput/VgaOutputV2.scala
+++ npc/playground/src/circuit_exp/CharacterInput/VgaOutputV2.scala
@@ -86,12 +86,15 @@ class Vmem extends Module {
   // cursor
   val cursor = Wire(Bool()) // 当前像素在光标上的标志位
   cursor := (io.x_addr_cnt === 0.U | io.x_addr_cnt === 1.U) & (io.x_addr === x_write_point) & (io.y_addr === y_write_point)
+  val cursor_white = RegInit(0.B)
+  val (cursor_cnt, cursor_change) = Counter(cursor, 511)
+  when(cursor_change){cursor_white := ~cursor_white}
 
   // 输出当前像素亮度
   val char_baseaddr_lut = VecInit((0 to 255) map {i => i.U * 16.U}) // 一个字符在dot_txt中占据16行
   val char_baseaddr = char_baseaddr_lut(ascii)  // 计算当前ascii在dot_txt中第几行开始
   val piexl_addr = char_baseaddr + io.y_addr_cnt  // 当前像素在dot_txt中对应第几行
-  val pixel_white = dot_txt(piexl_addr)(io.x_addr_cnt) & io.char || cursor
+  val pixel_white = dot_txt(piexl_addr)(io.x_addr_cnt) & io.char || cursor_white
 
   when(pixel_white.asBool()){
     io.vga_data := 0xffffff.U
