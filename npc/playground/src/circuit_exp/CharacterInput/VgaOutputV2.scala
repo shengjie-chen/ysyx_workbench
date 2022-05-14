486717fcc3fa16add925a06f1c546d3bb3ead016
Shengjie Chen <chenshengjie1999@126.com>
2022-05-14 23:53:23 +0800

    some bug to achieve arrow

--- npc/playground/src/circuit_exp/CharacterInput/VgaOutputV2.scala
+++ npc/playground/src/circuit_exp/CharacterInput/VgaOutputV2.scala
@@ -71,16 +71,16 @@ class Vmem extends Module {
         x_write_point := x_write_point - 1.U
       }
     }
-    //上下左右按键调整光标，不知道具体原因，这四个键码没使用第三套方案，和数字键盘重复，所以当方向键启用，则数字键盘4826无法使用
-      .elsewhen(io.keycode === 0x26.U){ // up arrow
-      y_write_point := y_write_point - 1.U
-    }.elsewhen(io.keycode === 0x25.U){ // left arrow
-      x_write_point := x_write_point - 1.U
-    }.elsewhen(io.keycode === 0x28.U){ // down arrow
-      y_write_point := y_write_point + 1.U
-    }.elsewhen(io.keycode === 0x27.U){ // right arrow
-      x_write_point := x_write_point + 1.U
-    }
+    //上下左右按键调整光标，不知道具体原因，这四个键码没使用第三套方案，可能会现有一个e0,之后才是键码，和数字键盘重复，暂时调不出来
+//      .elsewhen(io.keycode === 0x26.U){ // up arrow
+//      y_write_point := y_write_point - 1.U
+//    }.elsewhen(io.keycode === 0x25.U){ // left arrow
+//      x_write_point := x_write_point - 1.U
+//    }.elsewhen(io.keycode === 0x28.U){ // down arrow
+//      y_write_point := y_write_point + 1.U
+//    }.elsewhen(io.keycode === 0x27.U){ // right arrow
+//      x_write_point := x_write_point + 1.U
+//    }
       .otherwise{
       memory.write(write_point,io.write_data)
       when(y_write_point === 29.U && x_write_point === 69.U){ // 30 * 70
