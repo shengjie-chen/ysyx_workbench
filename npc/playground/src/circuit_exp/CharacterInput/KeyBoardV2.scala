2a746e163f7051b08a954446c44f88d7cc228843
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-14 11:00:27 +0800

    >  generate CharacterInput verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:00:27 up 58 min,  1 user,  load average: 0.93, 0.74, 0.57

--- npc/playground/src/circuit_exp/CharacterInput/KeyBoardV2.scala
+++ npc/playground/src/circuit_exp/CharacterInput/KeyBoardV2.scala
@@ -55,66 +55,68 @@ class KeyBoardV2 extends Module {
   val cnt_r = RegInit(0.U(8.W))
   cnt_r := cnt_vec(cnt)
 
+
+  when(u0.io.ready){
+    data0 := u0.io.data
+  }
+
   // key code to ascii rom
   val keycode2ascii = MuxCase(0.U(8.W),Array(
     //a - z
-    (u0.io.data === 0x1c.U) -> 97.U,
-    (u0.io.data === 0x32.U) -> 98.U,
-    (u0.io.data === 0x21.U) -> 99.U,
-    (u0.io.data === 0x23.U) -> 100.U,
-    (u0.io.data === 0x24.U) -> 101.U,
-    (u0.io.data === 0x2b.U) -> 102.U,
-    (u0.io.data === 0x34.U) -> 103.U,
-    (u0.io.data === 0x33.U) -> 104.U,
-    (u0.io.data === 0x43.U) -> 105.U,
-    (u0.io.data === 0x3b.U) -> 106.U,
-    (u0.io.data === 0x42.U) -> 107.U,
-    (u0.io.data === 0x4b.U) -> 108.U,
-    (u0.io.data === 0x3a.U) -> 109.U,
-    (u0.io.data === 0x31.U) -> 110.U,
-    (u0.io.data === 0x44.U) -> 111.U,
-    (u0.io.data === 0x4d.U) -> 112.U,
-    (u0.io.data === 0x15.U) -> 113.U,
-    (u0.io.data === 0x2d.U) -> 114.U,
-    (u0.io.data === 0x1b.U) -> 115.U,
-    (u0.io.data === 0x2c.U) -> 116.U,
-    (u0.io.data === 0x3c.U) -> 117.U,
-    (u0.io.data === 0x2a.U) -> 118.U,
-    (u0.io.data === 0x1d.U) -> 119.U,
-    (u0.io.data === 0x22.U) -> 120.U,
-    (u0.io.data === 0x35.U) -> 121.U,
-    (u0.io.data === 0x1a.U) -> 122.U,
+    (io.data === 0x1c.U) -> 97.U,
+    (io.data === 0x32.U) -> 98.U,
+    (io.data === 0x21.U) -> 99.U,
+    (io.data === 0x23.U) -> 100.U,
+    (io.data === 0x24.U) -> 101.U,
+    (io.data === 0x2b.U) -> 102.U,
+    (io.data === 0x34.U) -> 103.U,
+    (io.data === 0x33.U) -> 104.U,
+    (io.data === 0x43.U) -> 105.U,
+    (io.data === 0x3b.U) -> 106.U,
+    (io.data === 0x42.U) -> 107.U,
+    (io.data === 0x4b.U) -> 108.U,
+    (io.data === 0x3a.U) -> 109.U,
+    (io.data === 0x31.U) -> 110.U,
+    (io.data === 0x44.U) -> 111.U,
+    (io.data === 0x4d.U) -> 112.U,
+    (io.data === 0x15.U) -> 113.U,
+    (io.data === 0x2d.U) -> 114.U,
+    (io.data === 0x1b.U) -> 115.U,
+    (io.data === 0x2c.U) -> 116.U,
+    (io.data === 0x3c.U) -> 117.U,
+    (io.data === 0x2a.U) -> 118.U,
+    (io.data === 0x1d.U) -> 119.U,
+    (io.data === 0x22.U) -> 120.U,
+    (io.data === 0x35.U) -> 121.U,
+    (io.data === 0x1a.U) -> 122.U,
     // all Numeric keypad
-    (u0.io.data === 0x70.U) -> 48.U,//0
-    (u0.io.data === 0x69.U) -> 49.U,//1
-    (u0.io.data === 0x72.U) -> 50.U,//2
-    (u0.io.data === 0x7a.U) -> 51.U,//3
-    (u0.io.data === 0x6b.U) -> 52.U,//4
-    (u0.io.data === 0x73.U) -> 53.U,//5
-    (u0.io.data === 0x74.U) -> 54.U,//6
-    (u0.io.data === 0x6c.U) -> 55.U,//7
-    (u0.io.data === 0x75.U) -> 56.U,//8
-    (u0.io.data === 0x7d.U) -> 57.U,//9
+    (io.data === 0x70.U) -> 48.U,//0
+    (io.data === 0x69.U) -> 49.U,//1
+    (io.data === 0x72.U) -> 50.U,//2
+    (io.data === 0x7a.U) -> 51.U,//3
+    (io.data === 0x6b.U) -> 52.U,//4
+    (io.data === 0x73.U) -> 53.U,//5
+    (io.data === 0x74.U) -> 54.U,//6
+    (io.data === 0x6c.U) -> 55.U,//7
+    (io.data === 0x75.U) -> 56.U,//8
+    (io.data === 0x7d.U) -> 57.U,//9
     //
-    (u0.io.data === 0x5a.U) -> 13.U,//enter
-    (u0.io.data === 0x66.U) -> 8.U,//backspace
-    (u0.io.data === 0x29.U) -> 32.U//space
-    //    (io.data === 0x5a.U) -> 13.U,//enter
-    //    (io.data === 0x5a.U) -> 13.U,//enter
-    //    (io.data === 0x5a.U) -> 13.U,//enter
+    (io.data === 0x5a.U) -> 13.U,//enter
+    (io.data === 0x66.U) -> 8.U,//backspace
+    (io.data === 0x29.U) -> 32.U,//space
+//    (io.data === 0x5a.U) -> 13.U,//enter
+//    (io.data === 0x5a.U) -> 13.U,//enter
+//    (io.data === 0x5a.U) -> 13.U,//enter
+
 
   ))
-
-  when(u0.io.ready){
-    data0 := keycode2ascii
-  }
 
   io.data := data1
 
   // no_press posedge detect
   val no_press_r = Reg(UInt(3.W))
   no_press_r := Cat(no_press_r(1,0),no_press)
-  val no_press_posedge = !no_press_r(2) & no_press_r(1)
+  val no_press_posedge = ~no_press_r(2) & no_press_r(1)
   io.mem_write_en := no_press_posedge
 
 }
