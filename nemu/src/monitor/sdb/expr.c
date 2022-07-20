79f643c805f16b417a5d3716d9798f60f97c4fbd
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 22:01:28 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:01:28 up  1:07,  1 user,  load average: 0.91, 0.48, 0.35

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -91,7 +91,7 @@ static bool make_token(char *e)
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-        assert(nr_token < 32);
+        assert(nr_token < 1024);
         switch (rules[i].token_type) {
         case TK_NOTYPE:
           break;
