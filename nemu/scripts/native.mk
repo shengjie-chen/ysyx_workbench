dd4252b5785fc0bc89568aade8f58654a1d5aa52
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 11:38:17 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:38:17 up  2:11,  1 user,  load average: 0.38, 0.32, 0.20

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -26,6 +26,18 @@ gdb: run-env
 	$(call git_commit, "gdb NEMU")
 	gdb -s $(BINARY) --args $(NEMU_EXEC)
 
+INIT_C_LINE=17252
+INIT_H_LINE=1898
+CURR_C_LINE=$(shell find $(NEMU_HOME) -name "*.c" | xargs grep '^.'|wc -l)
+CURR_H_LINE=$(shell find $(NEMU_HOME) -name "*.h" | xargs grep '^.'|wc -l)
+NEW_C_LINE=$(shell echo $(CURR_C_LINE)-$(INIT_C_LINE) | bc)
+NEW_H_LINE=$(shell echo $(CURR_H_LINE)-$(INIT_H_LINE) | bc)
+count: 
+	@echo Total C file valid lines: $(CURR_C_LINE)
+	@echo New C file valid lines: $(NEW_C_LINE)
+	@echo Total H file valid lines: $(CURR_H_LINE)
+	@echo New H file valid lines: $(NEW_H_LINE)
+
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
 $(clean-tools):
 	-@$(MAKE) -s -C $@ clean
--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -24,7 +24,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+#ifdef CONFIG_WATCHPOINT
   check_watchpoint();
+#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
