8cc5ef4b5806ac85476f20f60252a893dba6f1c7
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-31 20:56:03 +0800

    >  generate RegisterFile verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:56:03 up  1:57,  1 user,  load average: 0.29, 0.58, 0.54

---
+++ npc/playground/src/RVNoob/RVNoob.scala
@@ -0,0 +1,5 @@
+package RVNoob
+
+class RVNoob {
+
+}
---
+++ npc/playground/src/RVNoob/RegisterFile.scala
@@ -0,0 +1,43 @@
+package RVNoob
+import chisel3._
+import math._
+
+class RegisterFile(
+  val ADDR_WIDTH: Int = 5,
+  val DATA_WIDTH: Int = 64)
+    extends Module {
+  val io = IO(new Bundle {
+    val wen    = Input(Bool())
+    val wdata  = Input(UInt(DATA_WIDTH.W))
+    val waddr  = Input(UInt(ADDR_WIDTH.W))
+    val ren1   = Input(Bool())
+    val ren2   = Input(Bool())
+    val rdata1 = Output(UInt(DATA_WIDTH.W))
+    val rdata2 = Output(UInt(DATA_WIDTH.W))
+    val raddr1 = Input(UInt(ADDR_WIDTH.W))
+    val raddr2 = Input(UInt(ADDR_WIDTH.W))
+  })
+  // init reg
+  val reg_num: Int = pow(2, ADDR_WIDTH).toInt
+  val rf = Vec(reg_num, Reg(UInt(DATA_WIDTH.W)))
+  rf(0) := 0.U
+
+  // read src1 and src2
+  val rdata1 = Wire(UInt(DATA_WIDTH.W))
+  val rdata2 = Wire(UInt(DATA_WIDTH.W))
+  rdata1    := rf(io.raddr1)
+  rdata2    := rf(io.raddr2)
+  io.rdata1 := Mux(io.ren1, rdata1, 0.U)
+  io.rdata2 := Mux(io.ren2, rdata2, 0.U)
+
+  // write reg
+  when(io.wen) {
+    rf(io.waddr) := io.wdata
+  }
+
+}
+
+object RegisterFileGen extends App {
+  (new chisel3.stage.ChiselStage)
+    .execute(Array("-td", "./build/RVNoob"), Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new RegisterFile())))
+}
