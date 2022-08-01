7bf3c8fcc237736f8fa4733b05ce4dca0779d1b2
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-01 19:31:46 +0800

    >  sim RVNoob RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:31:46 up  4:53,  1 user,  load average: 1.10, 0.63, 0.41

--- npc/playground/src/RVnpc/RVNoob/RVNoob.h
+++ npc/playground/src/RVnpc/RVNoob/RVNoob.h
@@ -30,3 +30,36 @@ typedef uint64_t paddr_t;
 #define CONFIG_PC_RESET_OFFSET 0x0
 //#define MUX_MACRO_PROPERTY(p, macro, a, b) MUX_WITH_COMMA(concat(p, macro), a, b)
 
+uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
+const uint32_t img [] = {
+  0xff010113,          	// addi	sp,sp,-16
+  0xfa010113,          	// addi	sp,sp,-96
+  0x004c8c93,          	// addi	s9,s9,4
+  0x00840413,          	// addi	s0,s0,8
+  0x004a0a13,          	// addi	s4,s4,4
+  0xff010113,          	// addi	sp,sp,-16
+  0xfa010113,          	// addi	sp,sp,-96
+  0x004c8c93,          	// addi	s9,s9,4
+  0x00840413,          	// addi	s0,s0,8
+  0x004a0a13,          	// addi	s4,s4,4
+//  0x0102b503,  // ld  a0,16(t0)
+//  0x00100073,  // ebreak (used as nemu_trap)
+//  0xdeadbeef,  // some data
+};
+
+uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
+
+word_t host_read(void *addr, int len) {
+  switch (len) {
+    case 1: return *(uint8_t  *)addr;
+    case 2: return *(uint16_t *)addr;
+    case 4: return *(uint32_t *)addr;
+    IFDEF(CONFIG_ISA64, case 8: return *(uint64_t *)addr);
+    default: MUXDEF(CONFIG_RT_CHECK, assert(0), return 0);
+  }
+}
+
+word_t pmem_read(paddr_t addr, int len) {
+  word_t ret = host_read(guest_to_host(addr), len);
+  return ret;
+}
