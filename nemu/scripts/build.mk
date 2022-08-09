ad9aff33de215df70cf36b0495788bfed8642110
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-09 19:37:11 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:37:11 up 43 min,  1 user,  load average: 1.14, 0.64, 0.45

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -22,11 +22,11 @@ CXX := g++
 endif
 LD := $(CXX)
 INCLUDES = $(addprefix -I, $(INC_PATH))
-CFLAGS  := -O2 -MMD -Wall -save-temps -Werror $(INCLUDES) $(CFLAGS)
+CFLAGS  := -O2 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
 -include $(NEMU_HOME)/include/config/auto.conf
-# ifdef CONFIG_SAVE_TEMPS
-# CFLAGS  += -save-temps
-# endif
+ifdef CONFIG_SAVE_TEMPS
+	CFLAGS  += -save-temps
+endif
 LDFLAGS := -O2 $(LDFLAGS)
 
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
