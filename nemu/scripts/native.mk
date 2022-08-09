dd157e9812e836026daf015642e9fe91dd49ca5b
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-09 10:39:25 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:39:25 up  1:47,  1 user,  load average: 0.62, 0.28, 0.29

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -11,7 +11,7 @@ $(BINARY): compile_git
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
-# override ARGS += --batch
+override ARGS += --batch
 
 # Command to execute NEMU
 IMG ?=
