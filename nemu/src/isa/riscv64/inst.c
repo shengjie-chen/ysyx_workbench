1908cf929fb7a40daab6becd5d8c263e922e3f5f
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-09 09:49:12 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:49:12 up 56 min,  1 user,  load average: 1.21, 0.58, 0.47

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -146,6 +146,6 @@ static int decode_exec(Decode *s) {
 
 int isa_exec_once(Decode *s) {
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
-  printf("%x\n",s->isa.inst.val);
+  // printf("%x\n",s->isa.inst.val);
   return decode_exec(s);
 }
