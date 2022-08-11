8422c6a28a4b26856fd50acacd9ca77ba9a3113c
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 20:06:55 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:06:55 up  7:08,  1 user,  load average: 0.81, 0.47, 0.32

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -15,7 +15,7 @@ char *ftrace_file = "/home/jiexxpu/ysyx/ysyx-workbench/nemu/build/nemu-mtrace-lo
 int mtrace_depth = 0;
 int mtrace_func_num;
 #define MAX_FUNC_NUM 100
-char *symname[MAX_FUNC_NUM];
+char symname[MAX_FUNC_NUM][20];
 vaddr_t symaddr[MAX_FUNC_NUM];
 
 void init_mtrace(const char *elf_file)
