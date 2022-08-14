34e2562e3fc1deee7833691177fc1f93ee00d6f8
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-14 20:49:51 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:49:51 up  3:03,  1 user,  load average: 0.33, 0.42, 0.43

--- npc/playground/src/RVnpc/RVNoob/IDU.scala
+++ npc/playground/src/RVnpc/RVNoob/IDU.scala
@@ -6,46 +6,45 @@ import chisel3.util._
 class IDU extends Module {
   val io = IO(new Bundle {
     val inst = Input(UInt(32.W))
-    val imm  = Output(UInt(64.W))
+    val imm = Output(UInt(64.W))
     // gpr
-    val wen  = Output(Bool())
-    val rd   = Output(UInt(5.W))
+    val wen = Output(Bool())
+    val rd = Output(UInt(5.W))
     val ren1 = Output(Bool())
     val ren2 = Output(Bool())
-    val rs1  = Output(UInt(5.W))
-    val rs2  = Output(UInt(5.W))
+    val rs1 = Output(UInt(5.W))
+    val rs2 = Output(UInt(5.W))
     // control
-    val add_en       = Output(Bool())
+    val add_en = Output(Bool())
     val alu_src1_mux = Output(Bool())
     val alu_src2_mux = Output(Bool())
-    val exe_out_mux  = Output(Bool())
-    val dnpc_mux     = Output(Bool())
-    val pc_mux       = Output(Bool())
-    val dnpc_0b0     = Output(Bool())
+    val exe_out_mux = Output(Bool())
+    val dnpc_mux = Output(Bool())
+    val pc_mux = Output(Bool())
+    val dnpc_0b0 = Output(Bool())
   })
   val opcode = Wire(UInt(7.W))
-  val fun3   = Wire(UInt(3.W))
+  val fun3 = Wire(UInt(3.W))
   opcode := io.inst(6, 0)
-  fun3   := io.inst(14, 12)
+  fun3 := io.inst(14, 12)
 
-  io.rd  := io.inst(11, 7)
+  io.rd := io.inst(11, 7)
   io.rs1 := io.inst(19, 15)
   io.rs2 := io.inst(24, 20)
 
   // inst
-  val rvi_addi  = opcode === "b0010011".U && fun3 === "b000".U
+  val rvi_addi = opcode === "b0010011".U && fun3 === "b000".U
   val rvi_auipc = opcode === "b0010111".U
-  val rvi_lui   = opcode === "b0110111".U
-  val rvi_jal   = opcode === "b1101111".U
-  val rvi_jalr  = opcode === "b1100111".U && fun3 === "b000".U
+  val rvi_lui = opcode === "b0110111".U
+  val rvi_jal = opcode === "b1101111".U
+  val rvi_jalr = opcode === "b1100111".U && fun3 === "b000".U
 
-  val inst_type = Wire(UInt(6.W))
-  val type_I    = rvi_addi || rvi_jalr // TYPE_I addi
-  val type_U    = rvi_auipc || rvi_lui // TYPE_U auipc
-  val type_S    = 0.B // TYPE_S
-  val type_J    = rvi_jal // TYPE_J
-  val type_R    = 0.B // TYPE_R
-  val type_B    = 0.B // TYPE_B
+  val type_I = rvi_addi || rvi_jalr // TYPE_I addi
+  val type_U = rvi_auipc || rvi_lui // TYPE_U auipc
+  val type_S = 0.B // TYPE_S
+  val type_J = rvi_jal // TYPE_J
+  val type_R = 0.B // TYPE_R
+  val type_B = 0.B // TYPE_B
 
   // imm
   val immI = Wire(UInt(64.W))
@@ -75,14 +74,14 @@ class IDU extends Module {
   )
 
   // control
-  io.add_en       := rvi_addi || rvi_auipc || rvi_jal || rvi_jalr
-  io.wen          := rvi_addi || rvi_auipc || rvi_lui || rvi_jal || rvi_jalr
-  io.ren1         := type_I || type_R || type_S || type_B
-  io.ren2         := type_S || type_R || type_B
+  io.add_en := rvi_addi || rvi_auipc || rvi_jal || rvi_jalr
+  io.wen := rvi_addi || rvi_auipc || rvi_lui || rvi_jal || rvi_jalr
+  io.ren1 := type_I || type_R || type_S || type_B
+  io.ren2 := type_S || type_R || type_B
   io.alu_src1_mux := type_I || type_R || type_S || type_B
   io.alu_src2_mux := type_S || type_R || type_B
-  io.exe_out_mux  := io.add_en
-  io.dnpc_mux     := rvi_jalr || rvi_auipc
-  io.dnpc_0b0     := rvi_jalr
-  io.pc_mux       := rvi_jal || rvi_jalr || rvi_auipc
+  io.exe_out_mux := io.add_en
+  io.dnpc_mux := rvi_jalr || rvi_auipc
+  io.dnpc_0b0 := rvi_jalr
+  io.pc_mux := rvi_jal || rvi_jalr || rvi_auipc
 }
