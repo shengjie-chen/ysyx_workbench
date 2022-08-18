41864a3399380339cca3dbb3429f89eb27b7422c
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-19 00:08:36 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:08:36 up  1:04,  1 user,  load average: 1.37, 1.14, 1.03

--- npc/playground/src/RVnpc/RVNoob/RVNoob_sim.cpp
+++ npc/playground/src/RVnpc/RVNoob/RVNoob_sim.cpp
@@ -65,11 +65,12 @@ int main(int argc, char **argv, char **env)
   }
   top->reset = 0;
 
+  printf("%d\n",argc);
+
   bool sdb_en = 0;
   if ((argv + 2) != NULL) {
     sdb_en = ~strcmp(*(argv + 2), "sdb");
   }
-  printf("!!!!\n");
   // printf("%s\n",*(argv + 2));
   // printf("%d\n",sdb_en);
   if (sdb_en) {
