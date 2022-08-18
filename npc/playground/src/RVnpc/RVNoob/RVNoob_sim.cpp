811dd2afd127cdce9e8627b2cc39ffa1f1a6504e
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 21:40:28 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:40:28 up 51 min,  1 user,  load average: 2.12, 1.03, 0.81

--- npc/playground/src/RVnpc/RVNoob/RVNoob_sim.cpp
+++ npc/playground/src/RVnpc/RVNoob/RVNoob_sim.cpp
@@ -18,7 +18,7 @@ NPCState npc_state;
 
 void npc_ebreak(){
   npc_state.state == NPC_END;
-  printf("!!!!!! npc ebreak !!!!!!\n")
+  printf("!!!!!! npc ebreak !!!!!!\n");
 }
 
 VRVNoob *top = new VRVNoob;
