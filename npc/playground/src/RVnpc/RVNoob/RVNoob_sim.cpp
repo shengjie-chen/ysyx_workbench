3f1a96d87007010a3a64bdbba217d377d494f87d
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 21:24:30 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:24:30 up 35 min,  1 user,  load average: 1.00, 1.06, 0.80

--- npc/playground/src/RVnpc/RVNoob/RVNoob_sim.cpp
+++ npc/playground/src/RVnpc/RVNoob/RVNoob_sim.cpp
@@ -15,7 +15,7 @@ int add(int a, int b) { return a + b; }
 vluint64_t main_time = 0;
 const vluint64_t sim_time = 100;
 NPCState NPC_state;
-NPC_state.state = NPC_RUNNING;
+NPCstate.state = NPC_RUNNING;
 
 int main(int argc, char **argv, char **env)
 {
