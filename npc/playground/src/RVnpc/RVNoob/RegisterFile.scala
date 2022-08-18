b90d35b61bb5cd49e781a6503882afe20753f0c8
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-18 22:14:00 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:14:00 up 22 min,  1 user,  load average: 1.04, 1.08, 0.91

--- npc/playground/src/RVnpc/RVNoob/RegisterFile.scala
+++ npc/playground/src/RVnpc/RVNoob/RegisterFile.scala
@@ -1,28 +1,60 @@
 package RVnpc.RVNoob
 
 import chisel3._
+import chisel3.util.HasBlackBoxInline
 
 import scala.math._
 
-class RegisterFile(
-  val ADDR_WIDTH: Int = 5,
-  val DATA_WIDTH: Int = 64)
-    extends Module {
+class RF_read(
+               val ADDR_WIDTH: Int = 5,
+               val DATA_WIDTH: Int = 64)
+  extends BlackBox with HasBlackBoxInline {
   val io = IO(new Bundle {
-    val wen    = Input(Bool())
-    val wdata  = Input(UInt(DATA_WIDTH.W))
-    val waddr  = Input(UInt(ADDR_WIDTH.W))
-    val ren1   = Input(Bool())
-    val ren2   = Input(Bool())
+    val rf = Input(Vec(pow(2, ADDR_WIDTH).toInt, UInt(DATA_WIDTH.W)))
+  })
+
+  setInline("rf_read.v",
+    """
+      |module rf_read(input [31:0] a,
+      |           input clk,
+      |           input reset,
+      |           output [3:0] b);
+      |
+      |  reg [3:0] b_temp;
+      |
+      |  always @ (posedge clk, negedge reset)
+      |    if(!reset)
+      |      b_temp <= 'b0;
+      |    else if(a == 'b0)
+      |      b_temp <= b_temp + 1'b1
+      |
+      |  assign b = b_temp;
+      |endmodule
+            """.stripMargin)
+
+}
+
+class RegisterFile(
+                    val ADDR_WIDTH: Int = 5,
+                    val DATA_WIDTH: Int = 64)
+  extends Module {
+  val io = IO(new Bundle {
+    val wen = Input(Bool())
+    val wdata = Input(UInt(DATA_WIDTH.W))
+    val waddr = Input(UInt(ADDR_WIDTH.W))
+    val ren1 = Input(Bool())
+    val ren2 = Input(Bool())
     val rdata1 = Output(UInt(DATA_WIDTH.W))
     val rdata2 = Output(UInt(DATA_WIDTH.W))
     val raddr1 = Input(UInt(ADDR_WIDTH.W))
     val raddr2 = Input(UInt(ADDR_WIDTH.W))
-    val a0     = Output(UInt(DATA_WIDTH.W))
+    val a0 = Output(UInt(DATA_WIDTH.W))
   })
   // init reg
   val reg_num: Int = pow(2, ADDR_WIDTH).toInt
   val rf = Reg(Vec(reg_num, UInt(DATA_WIDTH.W)))
+  val rf_read = Module(new RF_read)
+
   rf(0) := 0.U
   when(reset.asBool) {
     for (i <- 1 to 31) {
@@ -32,8 +64,8 @@ class RegisterFile(
   // read src1 and src2
   val rdata1 = Wire(UInt(DATA_WIDTH.W))
   val rdata2 = Wire(UInt(DATA_WIDTH.W))
-  rdata1    := rf(io.raddr1)
-  rdata2    := rf(io.raddr2)
+  rdata1 := rf(io.raddr1)
+  rdata2 := rf(io.raddr2)
   io.rdata1 := Mux(io.ren1, rdata1, 0.U)
   io.rdata2 := Mux(io.ren2, rdata2, 0.U)
 
