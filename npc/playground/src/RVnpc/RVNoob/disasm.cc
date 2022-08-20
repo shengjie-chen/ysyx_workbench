c611560bda087b9e419eb10cc0555af16f4e7b40
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-20 20:06:06 +0800

    >  sim RVNoob RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:06:06 up 13 min,  1 user,  load average: 0.41, 0.80, 0.92

--- npc/playground/src/RVnpc/RVNoob/disasm.cc
+++ npc/playground/src/RVnpc/RVNoob/disasm.cc
@@ -73,6 +73,8 @@ extern "C" void init_disasm(const char *triple) {
 }
 
 extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
+  printf("error\n");
+  
   MCInst inst;
   printf("error\n");
 
