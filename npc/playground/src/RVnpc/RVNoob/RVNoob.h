95455131dd2f39462ec7da43edc811911c20c608
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-21 09:28:32 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:28:32 up  1:22,  1 user,  load average: 1.35, 0.91, 0.85

--- npc/playground/src/RVnpc/RVNoob/RVNoob.h
+++ npc/playground/src/RVnpc/RVNoob/RVNoob.h
@@ -4,19 +4,19 @@
 #include "verilated_dpi.h"
 
 
-typedef signed char __int8_t;
-typedef unsigned char __uint8_t;
-typedef signed short int __int16_t;
-typedef unsigned short int __uint16_t;
-typedef signed int __int32_t;
-typedef unsigned int __uint32_t;
-typedef signed long int __int64_t;
-typedef unsigned long int __uint64_t;
+// typedef signed char __int8_t;
+// typedef unsigned char __uint8_t;
+// typedef signed short int __int16_t;
+// typedef unsigned short int __uint16_t;
+// typedef signed int __int32_t;
+// typedef unsigned int __uint32_t;
+// typedef signed long int __int64_t;
+// typedef unsigned long int __uint64_t;
 
-typedef __uint8_t uint8_t;
-typedef __uint16_t uint16_t;
-typedef __uint32_t uint32_t;
-typedef __uint64_t uint64_t;
+// typedef __uint8_t uint8_t;
+// typedef __uint16_t uint16_t;
+// typedef __uint32_t uint32_t;
+// typedef __uint64_t uint64_t;
 
 
 //#define PG_ALIGN __attribute((aligned(4096)))
