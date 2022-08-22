f537481eea8aaae52ff2f7a65a70fa78b3041928
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-22 20:15:53 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:15:53 up 53 min,  1 user,  load average: 1.27, 1.03, 0.68

--- npc/playground/src/RVnpc/RVNoob/RegisterFile.scala
+++ npc/playground/src/RVnpc/RVNoob/RegisterFile.scala
@@ -6,37 +6,36 @@ import chisel3.util.HasBlackBoxInline
 import scala.math._
 
 class RF_read(
-               val ADDR_WIDTH: Int = 5,
-               val DATA_WIDTH: Int = 64)
-  extends BlackBox {
+  val ADDR_WIDTH: Int = 5,
+  val DATA_WIDTH: Int = 64)
+    extends BlackBox {
   val io = IO(new Bundle {
     val rf = Input(Vec(pow(2, ADDR_WIDTH).toInt, UInt(DATA_WIDTH.W)))
   })
 }
 
 class RegisterFile(
-                    val ADDR_WIDTH: Int = 5,
-                    val DATA_WIDTH: Int = 64)
-  extends Module {
+  val ADDR_WIDTH: Int = 5,
+  val DATA_WIDTH: Int = 64)
+    extends Module {
   val io = IO(new Bundle {
-    val wen = Input(Bool())
-    val wdata = Input(UInt(DATA_WIDTH.W))
-    val waddr = Input(UInt(ADDR_WIDTH.W))
-    val ren1 = Input(Bool())
-    val ren2 = Input(Bool())
+    val wen    = Input(Bool())
+    val wdata  = Input(UInt(DATA_WIDTH.W))
+    val waddr  = Input(UInt(ADDR_WIDTH.W))
+    val ren1   = Input(Bool())
+    val ren2   = Input(Bool())
     val rdata1 = Output(UInt(DATA_WIDTH.W))
     val rdata2 = Output(UInt(DATA_WIDTH.W))
     val raddr1 = Input(UInt(ADDR_WIDTH.W))
     val raddr2 = Input(UInt(ADDR_WIDTH.W))
-    val a0 = Output(UInt(DATA_WIDTH.W))
+    val a0     = Output(UInt(DATA_WIDTH.W))
   })
   // init reg
   val reg_num: Int = pow(2, ADDR_WIDTH).toInt
-  val rf = Reg(Vec(reg_num, UInt(DATA_WIDTH.W)))
+  val rf      = Reg(Vec(reg_num, UInt(DATA_WIDTH.W)))
   val rf_read = Module(new RF_read)
   rf_read.io.rf <> rf
 
-  rf(0) := 0.U
   when(reset.asBool) {
     for (i <- 1 to 31) {
       rf(i) := i.U
@@ -45,8 +44,8 @@ class RegisterFile(
   // read src1 and src2
   val rdata1 = Wire(UInt(DATA_WIDTH.W))
   val rdata2 = Wire(UInt(DATA_WIDTH.W))
-  rdata1 := rf(io.raddr1)
-  rdata2 := rf(io.raddr2)
+  rdata1    := rf(io.raddr1)
+  rdata2    := rf(io.raddr2)
   io.rdata1 := Mux(io.ren1, rdata1, 0.U)
   io.rdata2 := Mux(io.ren2, rdata2, 0.U)
 
@@ -56,6 +55,7 @@ class RegisterFile(
   }
 
   io.a0 := rf(10)
+  rf(0) := 0.U
 
 }
 
