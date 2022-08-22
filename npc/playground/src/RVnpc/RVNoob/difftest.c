ae1ded87f60587a3217adfb77e20579a2e308157
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-22 20:09:35 +0800

    >  sim RVNoob RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:09:35 up 47 min,  1 user,  load average: 0.51, 0.53, 0.47

--- npc/playground/src/RVnpc/RVNoob/difftest.c
+++ npc/playground/src/RVnpc/RVNoob/difftest.c
@@ -59,18 +59,18 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
 {
   for (int i = 0; i < 32; i++) {
     if (cpu_state.gpr[i] != ref_r->gpr[i]) {
-      printf("!!!!!!!");
+      printf("!!!!!!!\n");
       printf("cpu.gpr[%d] is " FMT_WORD "\n", i, cpu_state.gpr[i]);
       printf("ref.gpr[%d] is " FMT_WORD "\n", i, ref_r->gpr[i]);
-      printf("!!!!!!!");
+      printf("!!!!!!!\n");
       return false;
     }
   }
   if (pc != ref_r->pc) {
-    printf("!!!!!!!");
+    printf("!!!!!!!\n");
     printf("cpu.pc is " FMT_WORD "\n", cpu_state.pc);
     printf("ref.pc is " FMT_WORD "\n", ref_r->pc);
-    printf("!!!!!!!");
+    printf("!!!!!!!\n");
     return false;
   }
   return true;
