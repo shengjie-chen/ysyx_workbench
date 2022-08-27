b8d83ee8aedde3a6fa122884d856f9829a792217
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-27 14:08:55 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:08:55 up  7:31,  1 user,  load average: 0.49, 0.74, 0.71

---
+++ npc/obj_dir/VRVNoob__Dpi.h
@@ -0,0 +1,31 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Prototypes for DPI import and export functions.
+//
+// Verilator includes this file in all generated .cpp files that use DPI functions.
+// Manually include this file where DPI .c import functions are declared to ensure
+// the C functions match the expectations of the DPI imports.
+
+#include "svdpi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    // DPI IMPORTS
+    // DPI import at /home/jiexxpu/ysyx/ysyx-workbench/npc/build/RVnpc/RVNoob/Verilog_Gen/DpiInst.v:3:30
+    extern void inst_change(const svLogicVecVal* a);
+    // DPI import at /home/jiexxpu/ysyx/ysyx-workbench/npc/build/RVnpc/RVNoob/Verilog_Gen/DpiNpc.v:2:30
+    extern void npc_change(const svLogicVecVal* a);
+    // DPI import at /home/jiexxpu/ysyx/ysyx-workbench/npc/playground/src/RVnpc/RVNoob/Ebreak.v:2:30
+    extern void npc_ebreak();
+    // DPI import at /home/jiexxpu/ysyx/ysyx-workbench/npc/build/RVnpc/RVNoob/Verilog_Gen/DpiPmem.v:11:30
+    extern void pmem_read(long long raddr, long long* rdata);
+    // DPI import at /home/jiexxpu/ysyx/ysyx-workbench/npc/build/RVnpc/RVNoob/Verilog_Gen/DpiPmem.v:13:30
+    extern void pmem_write(long long waddr, long long wdata, char wmask);
+    // DPI import at /home/jiexxpu/ysyx/ysyx-workbench/npc/playground/src/RVnpc/RVNoob/RF_read.v:1:30
+    extern void set_gpr_ptr(const svOpenArrayHandle a);
+
+#ifdef __cplusplus
+}
+#endif
