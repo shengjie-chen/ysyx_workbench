2d1ee927f1f39877c039373585ee19b5875525b1
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-28 22:34:31 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:34:31 up  8:30,  1 user,  load average: 0.72, 0.39, 0.29

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,11 +1,16 @@
 #include <am.h>
 #include <nemu.h>
+#define CONFIG_RTC_MMIO 0xa0000048
+
+static uint64_t boot_time;
 
 void __am_timer_init() {
+  boot_time = (uint64_t)inl(CONFIG_RTC_MMIO) | (((uint64_t)inl(CONFIG_RTC_MMIO+4))<<32);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 1000000;
+  uint64_t now = (uint64_t)inl(CONFIG_RTC_MMIO) | (((uint64_t)inl(CONFIG_RTC_MMIO+4))<<32);
+  uptime->us = now - boot_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
