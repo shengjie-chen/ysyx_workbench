bb5646d67719247628e61f3f5b9bb88c63113a83
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-31 17:40:01 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:40:01 up  1:48,  1 user,  load average: 0.50, 0.35, 0.33

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,6 +3,8 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+int width;
+int height;
 void __am_gpu_init()
 {
   // int i;
@@ -12,6 +14,9 @@ void __am_gpu_init()
   // for (i = 0; i < w * h; i++)
   //   fb[i] = i;
   // outl(SYNC_ADDR, 1);
+
+  width = io_read(AM_GPU_CONFIG).width;
+  height = io_read(AM_GPU_CONFIG).height;
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
@@ -23,21 +28,20 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
 {
   if (ctl->sync) {
-    int width = io_read(AM_GPU_CONFIG).width;
-    int height = io_read(AM_GPU_CONFIG).height;
     int x1, x2, y1, y2;
     x1 = ctl->x;
     y1 = ctl->y;
     x2 = x1 + ctl->w;
     y2 = y1 + ctl->h;
+    int i, j;
     uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-    for (int i = 0; i < height; i++) {
-      for (int j = 0; j < width; j++) {
-        if ((j == x1) || (j == x2) || (i == y1) || (i == y2)) {
+    for (i = 0; i < height; i++) {
+      for (j = 0; j < width; j++) {
+        if ((j == x1) || (j == x2) || (i == y1) || (i == y2))
           *(fb + i * width + j) = 0x00ff0000;
-        } else {
-          *(fb + i * width + j) = 0x00000000;
-        }
+        // else {
+        //   *(fb + i * width + j) = 0x00000000;
+        // }
       }
     }
     outl(SYNC_ADDR, 1);
