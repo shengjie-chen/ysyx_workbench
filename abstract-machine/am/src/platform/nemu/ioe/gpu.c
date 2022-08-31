ad5689d33b39f9970a2f0ce5a2ee5e0ada40de5d
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-31 23:14:44 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:14:44 up  3:22,  1 user,  load average: 0.66, 1.02, 0.84

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -28,31 +28,28 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
 {
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *color_buf = ctl->pixels;
   if (!ctl->sync) {
     int x, y, w, h;
     x = ctl->x;
     y = ctl->y;
     w = ctl->w;
     h = ctl->h;
-    uint32_t *color_buf = ctl->pixels;
     int i, j;
-    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
     for (i = 0; i < h; i++) {
       for (j = 0; j < w; j++) {
         *(fb + (i + y) * width + j + x) = color_buf[i * w + j];
       }
     }
-  } 
-  // else {
-  //   uint32_t *color_buf = ctl->pixels;
-  //   for (i = 0; i < height; i++) {
-  //     for (j = 0; j < width; j++) {
-  //       *(fb + i * width + j) = color_buf[i * width + j];
-  //     }
-  //   }
-  // }
-    outl(SYNC_ADDR, 1);
-
+  } else {
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        *(fb + i * width + j) = color_buf[i * width + j];
+      }
+    }
+  }
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status)
