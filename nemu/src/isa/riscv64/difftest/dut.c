9a7edd0c7d22e99ddf6d9aa1c0a9f1705156e11f
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-31 23:53:03 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:53:03 up  4:01,  1 user,  load average: 0.52, 0.42, 0.56

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -6,10 +6,16 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
 {
   for (int i = 0; i < 32; i++) {
     if (gpr(i) != ref_r->gpr[i]) {
+      printf("difftest fail! gpr[%d] is different!\n", i);
+      printf("ref.gpr[%d] is " FMT_WORD "\n", i, ref_r->gpr[i]);
+      printf("cpu.gpr[%d] is " FMT_WORD "\n", i, cpu.gpr[i]);
       return false;
     }
   }
   if (cpu.pc != ref_r->pc) {
+    printf("difftest fail! pc is different!\n");
+    printf("ref.pc is " FMT_WORD "\n", ref_r->pc);
+    printf("cpu.pc is " FMT_WORD "\n", cpu.pc);
     return false;
   }
   return true;
