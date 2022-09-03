ae1b9864807e327144d804e8815fb3b1e4c25cf5
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-03 17:02:04 +0800

    >  sim RVNoob RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:02:04 up  1:00,  1 user,  load average: 0.45, 0.81, 0.82

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,5 +1,19 @@
 #include <am.h>
 #include <klib-macros.h>
+#include "../riscv.h"
+
+# define DEVICE_BASE 0xa0000000
+#define MMIO_BASE 0xa0000000
+
+#define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)
+#define KBD_ADDR        (DEVICE_BASE + 0x0000060)
+#define RTC_ADDR        (DEVICE_BASE + 0x0000048)
+#define VGACTL_ADDR     (DEVICE_BASE + 0x0000100)
+#define AUDIO_ADDR      (DEVICE_BASE + 0x0000200)
+#define DISK_ADDR       (DEVICE_BASE + 0x0000300)
+#define FB_ADDR         (MMIO_BASE   + 0x1000000)
+#define AUDIO_SBUF_ADDR (MMIO_BASE   + 0x1200000)
+
 
 extern char _heap_start;
 int main(const char *args);
@@ -17,7 +31,7 @@ static const char mainargs[] = MAINARGS;
 # define npc_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
 
 void putch(char ch) {
-  
+  outb(SERIAL_PORT, ch);
 }
 
 void halt(int code) {
