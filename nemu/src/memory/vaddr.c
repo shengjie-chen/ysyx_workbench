d61434e3086d554036e4da1f8f97ef40966d0523
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-03 10:57:46 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:57:46 up 23 min,  1 user,  load average: 0.44, 0.40, 0.39

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -1,14 +1,20 @@
 #include <isa.h>
 #include <memory/paddr.h>
-
-word_t vaddr_ifetch(vaddr_t addr, int len) {
+extern FILE *mtrace_fp;
+word_t vaddr_ifetch(vaddr_t addr, int len)
+{
+#ifdef CONFIG_MTRACE
+  fprintf(mtrace_fp, "ifetch: ");
+#endif
   return paddr_read(addr, len);
 }
 
-word_t vaddr_read(vaddr_t addr, int len) {
+word_t vaddr_read(vaddr_t addr, int len)
+{
   return paddr_read(addr, len);
 }
 
-void vaddr_write(vaddr_t addr, int len, word_t data) {
+void vaddr_write(vaddr_t addr, int len, word_t data)
+{
   paddr_write(addr, len, data);
 }
