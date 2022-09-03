921f7a8d7bb85e1a0f25be842aee24bca7475610
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-03 13:13:24 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:13:24 up 58 min,  1 user,  load average: 0.53, 0.53, 0.69

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -6,8 +6,7 @@
 
 int width;
 int height;
-void __am_gpu_init()
-{
+void __am_gpu_init() {
   // int i;
   // int w = io_read(AM_GPU_CONFIG).width;
   // int h = io_read(AM_GPU_CONFIG).height;
@@ -20,16 +19,41 @@ void __am_gpu_init()
   height = io_read(AM_GPU_CONFIG).height;
 }
 
-void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
-{
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T){
       .present = true, .has_accel = false, .width = inw(VGACTL_ADDR + 2), .height = inw(VGACTL_ADDR), .vmemsz = 0};
 }
 
-void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
-{
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 
-  if (!ctl->sync) {
+  // if (!ctl->sync) {
+  //   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  //   uint32_t *color_buf = ctl->pixels;
+  //   int x, y, w, h;
+  //   x = ctl->x;
+  //   y = ctl->y;
+  //   w = ctl->w;
+  //   h = ctl->h;
+  //   int i, j;
+  //   for (i = 0; i < h; i++) {
+  //     for (j = 0; j < w; j++) {
+  //       *(fb + (i + y) * width + j + x) = color_buf[i * w + j];
+  //     }
+  //   }
+  // } else {
+  //   if (ctl->pixels != NULL) {
+  //     uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  //     uint32_t *color_buf = ctl->pixels;
+  //     for (int i = 0; i < height; i++) {
+  //       for (int j = 0; j < width; j++) {
+  //         *(fb + i * width + j) = color_buf[i * width + j];
+  //       }
+  //     }
+  //   }
+  //   outl(SYNC_ADDR, 1);
+  // }
+
+  if (ctl->pixels != NULL) {
     uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
     uint32_t *color_buf = ctl->pixels;
     int x, y, w, h;
@@ -43,21 +67,12 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
         *(fb + (i + y) * width + j + x) = color_buf[i * w + j];
       }
     }
-  } else {
-    if (ctl->pixels != NULL) {
-      uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-      uint32_t *color_buf = ctl->pixels;
-      for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-          *(fb + i * width + j) = color_buf[i * width + j];
-        }
-      }
-    }
+  }
+  if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status)
-{
+void __am_gpu_status(AM_GPU_STATUS_T *status) {
   status->ready = true;
 }
