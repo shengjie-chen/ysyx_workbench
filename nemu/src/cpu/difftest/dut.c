2de9229d177cfb44db0f63ea9923e07404d5caa9
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-08 01:47:06 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:47:06 up 46 min,  1 user,  load average: 0.56, 0.34, 0.29

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -91,7 +91,6 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
     if (ref_r.pc == npc) {
       skip_dut_nr_inst = 0;
-      printf("%lx\n",npc);
       checkregs(&ref_r, npc);
       return;
     }
