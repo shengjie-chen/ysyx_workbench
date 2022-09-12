fe945f8b24eb4675b3070bb60ff59aea88556c7b
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-12 21:01:35 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:01:35 up  1:45,  1 user,  load average: 0.83, 0.62, 0.55

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -4,19 +4,22 @@
 static uint64_t boot_time;
 
 void __am_timer_init() {
-  boot_time = (uint64_t)inl(RTC_ADDR) | (((uint64_t)inl(RTC_ADDR+4))<<32);
+  // boot_time = (uint64_t)inl(RTC_ADDR) | (((uint64_t)inl(RTC_ADDR+4))<<32);
+  boot_time = (uint64_t)ind(RTC_ADDR);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint64_t now = (uint64_t)inl(RTC_ADDR) | (((uint64_t)inl(RTC_ADDR+4))<<32);
+  // uint64_t now = (uint64_t)inl(RTC_ADDR) | (((uint64_t)inl(RTC_ADDR+4))<<32);
+  // uptime->us = now - boot_time;
+  uint64_t now = (uint64_t)ind(RTC_ADDR);
   uptime->us = now - boot_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->second = 0;
   rtc->minute = 0;
-  rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
-  rtc->year   = 1900;
+  rtc->hour = 0;
+  rtc->day = 0;
+  rtc->month = 0;
+  rtc->year = 1900;
 }
