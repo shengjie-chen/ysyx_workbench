f555683f599a8f0021f00932f06e8161b3ec4e58
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-18 17:28:38 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:28:38 up  5:28,  1 user,  load average: 0.25, 0.38, 0.32

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,11 +5,11 @@ static Context *do_event(Event e, Context *c) {
   case EVENT_YIELD : {
   for (int i = 1; i < 35; i++) {
       if (i != 2)
-        printf("gpr x%d : %lx", i, c->gpr[i]);
+        printf("gpr x%d : %lx\n", i, c->gpr[i]);
     }
-    printf("mcause  : %lx", c->mcause);
-    printf("mstatus : %lx", c->mstatus);
-    printf("mepc    : %lx", c->mepc);}
+    printf("mcause  : %lx\n", c->mcause);
+    printf("mstatus : %lx\n", c->mstatus);
+    printf("mepc    : %lx\n", c->mepc);}
     break;
   default:
     panic("Unhandled event ID = %d", e.event);
