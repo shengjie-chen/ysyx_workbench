db4b77a83a620813be43e52952ea1292b38e4d51
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-21 22:12:11 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:12:11 up  1:05,  1 user,  load average: 0.14, 0.29, 0.35

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <assert.h>
 
+extern void _exit(int status);
+
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   environ = empty;
-  exit(main(0, empty, empty));
+  _exit(main(0, empty, empty));
   assert(0);
 }
