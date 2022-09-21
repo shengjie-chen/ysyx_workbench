88af6fd5da648062e63ae4d354e5dd44c1e8105c
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-21 11:44:19 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:44:19 up  2:47,  1 user,  load average: 0.58, 0.32, 0.28

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -40,18 +40,14 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   uintptr_t entry = 0;
   Elf_Phdr elf_seg;
-  int j = 0;
   for (int i = 0; i < elf_head.e_phnum; i++) {
     ramdisk_read(&elf_seg, elf_head.e_phoff + i * elf_head.e_phentsize, sizeof(Elf_Phdr));
     if (elf_seg.p_type == PT_LOAD) {
-      if (j == 0) {
-        entry = elf_seg.p_vaddr;
-        j++;
-      }
       memcpy((void *)elf_seg.p_vaddr, elf_head.e_phoff + elf_head.e_phnum * elf_head.e_phentsize + (&ramdisk_start), elf_seg.p_filesz);
       memset((void *)(elf_seg.p_vaddr + elf_seg.p_filesz), 0, elf_seg.p_memsz - elf_seg.p_filesz);
     }
   }
+  entry = elf_head.e_entry;
   return entry;
 }
 
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,9 +3,13 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   switch (a[0]) {
     case SYS_yield: yield();c->GPRx = 0;break;
+    case SYS_exit: halt(a[1]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
