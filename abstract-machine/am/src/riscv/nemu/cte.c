69ebc9d134a8b042f1db3bc605bf01b373cd3a11
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-21 10:38:10 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:38:10 up  1:40,  1 user,  load average: 0.22, 0.27, 0.22

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -1,15 +1,23 @@
+#include "/home/jiexxpu/ysyx/ysyx-workbench/navy-apps/libs/libos/src/syscall.h"
 #include <am.h>
-#include <riscv/riscv.h>
 #include <klib.h>
+#include <riscv/riscv.h>
+static Context *(*user_handler)(Event, Context *) = NULL;
 
-static Context* (*user_handler)(Event, Context*) = NULL;
-
-Context* __am_irq_handle(Context *c) {
+Context *__am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 11: ev.event = EVENT_YIELD; break;
-      default: ev.event = EVENT_ERROR; break;
+    case 11: {
+      register intptr_t type asm("a7");
+      if (type == SYS_exit)
+        ev.event = EVENT_SYSCALL;
+      else
+        ev.event = EVENT_YIELD;
+    } break;
+    default:
+      ev.event = EVENT_ERROR;
+      break;
     }
     c = user_handler(ev, c);
     assert(c != NULL);
@@ -19,9 +27,11 @@ Context* __am_irq_handle(Context *c) {
 
 extern void __am_asm_trap(void);
 
-bool cte_init(Context*(*handler)(Event, Context*)) {
+bool cte_init(Context *(*handler)(Event, Context *)) {
   // initialize exception entry
-  asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
+  asm volatile("csrw mtvec, %0"
+               :
+               : "r"(__am_asm_trap));
 
   // register event handler
   user_handler = handler;
