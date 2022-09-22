0a732f5195443f13fe4f77afd68c0c19138d8aba
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-22 15:43:40 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:43:40 up  5:42,  1 user,  load average: 0.55, 0.28, 0.22

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,17 +13,17 @@ void do_syscall(Context *c) {
   a[3] = c->GPR4;
 
   // write_strace(a);
-  printf("\n[  log  ]system call ! ");
-  printf("a0:%ld, a1:%ld, a2:%lx, a3:%ld  ", a[0], a[1], a[2], a[3]);
+  // printf("\n[  log  ]system call ! ");
+  // printf("a0:%ld, a1:%ld, a2:%lx, a3:%ld  ", a[0], a[1], a[2], a[3]);
 
   switch (a[0]) {
   case SYS_brk: {
-    printf("name : %s", "SYS_brk\n");
+    // printf("name : %s", "SYS_brk\n");
     malloc(a[1]);
     c->GPRx = 0;
   } break;
   case SYS_write: {
-    printf("name : %s", "SYS_write\n");
+    // printf("name : %s", "SYS_write\n");
     int i = 0;
     if (a[1] == 1 || a[1] == 2) {
       for (i = 0; i < a[3]; i++) {
@@ -33,14 +33,12 @@ void do_syscall(Context *c) {
     c->GPRx = i;
   } break;
   case SYS_yield:
-    printf("name : %s", "SYS_yield\n");
-
+    // printf("name : %s", "SYS_yield\n");
     yield();
     c->GPRx = 0;
     break;
   case SYS_exit:
-    printf("name : %s", "SYS_exit\n");
-
+    // printf("name : %s", "SYS_exit\n");
     // printf("%x\n", 0);
     // asm volatile ("ebreak");
     halt(a[1]);
