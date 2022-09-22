e2407ab25049aefce519f82c7739a88379da205b
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-22 14:03:47 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:03:47 up  4:05,  1 user,  load average: 0.42, 0.34, 0.41

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -1,19 +1,20 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
   while (1) {
-    j ++;
+    j++;
     if (j == 10000) {
-      printf("%d", i);
-      printf("\n");
-      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-      j = 0;
-    break;
+      write(1, "Hello World!\n", 13);
 
+      // printf("%d", i);
+      // printf("\n");
+      printf("Hello World from Navy-apps for the %dth time!\n", i++);
+      j = 0;
+      break;
     }
   }
   return 0;
