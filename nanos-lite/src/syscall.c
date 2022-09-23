117942aaf688141fd37d6ab2aacd34d93063af0a
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-23 15:50:55 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:50:55 up  5:09,  1 user,  load average: 0.91, 0.63, 0.41

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -66,10 +66,10 @@ void do_syscall(Context *c) {
     c->GPRx = 0;
   } break;
   case SYS_gettimeofday: {
-    printf("name : %s", "SYS_gettimeofday\n");
     long int ms = io_read(AM_TIMER_UPTIME).us / 1000;
     *(long int *)a[1] = ms / 1000;
     *((long int *)a[1] + 1) = ms % 1000;
+    printf("name : %s, get time : %lds %ldms\n", "SYS_gettimeofday",*(long int *)a[1],*((long int *)a[1] + 1));
     c->GPRx = 0;
   } break;
 
