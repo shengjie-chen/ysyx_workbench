2b9829d3879690e7c2966714abf4a020aea7df95
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-23 10:43:28 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:43:28 up  1:36,  1 user,  load average: 0.10, 0.17, 0.28

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,6 @@
 #include "syscall.h"
-#include <common.h>
 #include "fs.h"
+#include <common.h>
 // #include <stdio.h>
 // #ifdef CONFIG_STRACE
 // extern void write_strace(uintptr_t *a);
@@ -16,42 +16,56 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
-// #ifdef CONFIG_STRACE
+  // #ifdef CONFIG_STRACE
   // write_strace(a);
   printf("\n[  log  ]system call ! ");
   printf("a0:%ld, a1:%ld, a2:%lx, a3:%ld  ", a[0], a[1], a[2], a[3]);
-// #endif
+  // #endif
   switch (a[0]) {
-    case SYS_open: {
+  case SYS_exit:
+    printf("name : %s", "SYS_exit\n");
+    halt(a[1]);
+    break;
+  case SYS_yield:
+    printf("name : %s", "SYS_yield\n");
+    yield();
+    c->GPRx = 0;
+    break;
+  case SYS_open: {
     printf("name : %s", "SYS_open\n");
-    c->GPRx = fs_open((char *)a[1],0,0);
+    c->GPRx = fs_open((char *)a[1], 0, 0);
+  } break;
+
+  case SYS_read: {
+    printf("name : %s", "SYS_read\n");
+    c->GPRx = fs_read(a[1],(void *)a[2], a[3]);
+  } break;
+  case SYS_write: {
+    printf("name : %s", "SYS_write\n");
+    // int i = 0;
+    // if (a[1] == 1 || a[1] == 2) {
+    //   for (i = 0; i < a[3]; i++) {
+    //     putch(*((char *)a[2] + i));
+    //   }
+    // }
+    // c->GPRx = i;
+    c->GPRx = fs_write(a[1],(void *)a[2], a[3]);
+  } break;
+
+  case SYS_close: {
+    printf("name : %s", "SYS_close\n");
+    c->GPRx = fs_close(a[1]);
+  } break;
+  case SYS_lseek: {
+    printf("name : %s", "SYS_lseek\n");
+    c->GPRx = fs_lseek(a[1],a[2], a[3]);
   } break;
   case SYS_brk: {
     printf("name : %s", "SYS_brk\n");
     malloc(a[1]);
     c->GPRx = 0;
   } break;
-  case SYS_write: {
-    printf("name : %s", "SYS_write\n");
-    int i = 0;
-    if (a[1] == 1 || a[1] == 2) {
-      for (i = 0; i < a[3]; i++) {
-        putch(*((char *)a[2] + i));
-      }
-    }
-    c->GPRx = i;
-  } break;
-  case SYS_yield:
-    printf("name : %s", "SYS_yield\n");
-    yield();
-    c->GPRx = 0;
-    break;
-  case SYS_exit:
-    printf("name : %s", "SYS_exit\n");
-    // printf("%x\n", 0);
-    // asm volatile ("ebreak");
-    halt(a[1]);
-    break;
+
   default:
     panic("Unhandled syscall ID = %d", a[0]);
   }
