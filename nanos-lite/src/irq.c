e6940328d5984957d35c57b0f2c0688d95dce3ed
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-23 16:11:15 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:11:15 up  5:29,  1 user,  load average: 0.47, 0.43, 0.44

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ extern void do_syscall(Context *c);
 static Context *do_event(Event e, Context *c) {
   switch (e.event) {
   case EVENT_SYSCALL:
-    printf("syscall!!\n");
+    // printf("syscall!!\n");
     do_syscall(c);
     break;
   case EVENT_YIELD:
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,6 +8,12 @@
 
 // extern FILE *strace_fp;
 // extern void write_strace(uintptr_t *a);
+#define __IGNORE(...)
+#define CHOOSE2nd(a, b, ...) b
+#define MUX_WITH_COMMA(contain_comma, a, b) CHOOSE2nd(contain_comma a, b)
+#define MUX_MACRO_PROPERTY(p, macro, a, b) MUX_WITH_COMMA(concat(p, macro), a, b)
+#define MUXDEF(macro, X, Y)  MUX_MACRO_PROPERTY(__P_DEF_, macro, X, Y)
+#define IFDEF(macro, ...) MUXDEF(macro, __KEEP, __IGNORE)(__VA_ARGS__)
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
@@ -16,32 +22,32 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
-  // #ifdef CONFIG_STRACE
+#ifdef CONFIG_STRACE
   // write_strace(a);
   printf("\n[  log  ]system call ! ");
   printf("a0:%ld, a1:%ld, a2:%lx, a3:%ld  ", a[0], a[1], a[2], a[3]);
-  // #endif
+#endif
   switch (a[0]) {
   case SYS_exit:
-    printf("name : %s", "SYS_exit\n");
+    IFDEF(CONFIG_STRACE, printf("name : %s", "SYS_exit\n"));
     halt(a[1]);
     break;
   case SYS_yield:
-    printf("name : %s", "SYS_yield\n");
+    IFDEF(CONFIG_STRACE, printf("name : %s", "SYS_yield\n"));
     yield();
     c->GPRx = 0;
     break;
   case SYS_open: {
-    printf("name : %s", "SYS_open\n");
+    IFDEF(CONFIG_STRACE, printf("name : %s", "SYS_open\n"));
     c->GPRx = fs_open((char *)a[1], 0, 0);
   } break;
 
   case SYS_read: {
     c->GPRx = fs_read(a[1], (void *)a[2], a[3]);
-    printf("name : %s, data num: %d\n", "SYS_read", c->GPRx);
+    IFDEF(CONFIG_STRACE, printf("name : %s, data num: %d\n", "SYS_read", c->GPRx));
   } break;
   case SYS_write: {
-    printf("name : %s", "SYS_write\n");
+    IFDEF(CONFIG_STRACE, printf("name : %s", "SYS_write\n"));
     // int i = 0;
     // if (a[1] == 1 || a[1] == 2) {
     //   for (i = 0; i < a[3]; i++) {
@@ -53,15 +59,15 @@ void do_syscall(Context *c) {
   } break;
 
   case SYS_close: {
-    printf("name : %s", "SYS_close\n");
+    IFDEF(CONFIG_STRACE, printf("name : %s", "SYS_close\n"));
     c->GPRx = fs_close(a[1]);
   } break;
   case SYS_lseek: {
     c->GPRx = fs_lseek(a[1], a[2], a[3]);
-    printf("name : %s, offset : 0x%x\n", "SYS_lseek", c->GPRx);
+    IFDEF(CONFIG_STRACE, printf("name : %s, offset : 0x%x\n", "SYS_lseek", c->GPRx));
   } break;
   case SYS_brk: {
-    printf("name : %s, malloc : %d\n", "SYS_brk", a[1]);
+    IFDEF(CONFIG_STRACE, printf("name : %s, malloc : %d\n", "SYS_brk", a[1]));
     malloc(a[1]);
     c->GPRx = 0;
   } break;
@@ -69,7 +75,7 @@ void do_syscall(Context *c) {
     long int ms = io_read(AM_TIMER_UPTIME).us / 1000;
     *(long int *)a[1] = ms / 1000;
     *((long int *)a[1] + 1) = ms % 1000;
-    printf("name : %s, get time : %lds %ldms\n", "SYS_gettimeofday",*(long int *)a[1],*((long int *)a[1] + 1));
+    IFDEF(CONFIG_STRACE, printf("name : %s, get time : %lds %ldms\n", "SYS_gettimeofday", *(long int *)a[1], *((long int *)a[1] + 1)));
     c->GPRx = 0;
   } break;
 
