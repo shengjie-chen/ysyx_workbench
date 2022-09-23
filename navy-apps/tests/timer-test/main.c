77398045e4b5590bb4ca850ebb6966efde987009
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-23 15:44:00 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:44:00 up  5:03,  1 user,  load average: 0.39, 0.33, 0.28

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -4,16 +4,19 @@
 #include <time.h>
 
 int main() {
-  struct timeval *last_time = {0};
-  struct timeval *curr_time = {0};
+  struct timeval *last_time;
+  struct timeval *curr_time;
+  printf("%p\n",last_time);
   _gettimeofday(last_time, NULL);
   int i = 0;
   while (1) {
     i++;
     _gettimeofday(curr_time, NULL);
     long int t_interval = (curr_time->tv_sec - last_time->tv_sec) * 1000 + (curr_time->tv_usec - last_time->tv_usec);
-    if (t_interval > 500)
+    if (t_interval > 500){
       printf("time pass more than 0.5s in %d time", i);
+      *last_time = *curr_time;
+      }
   }
   return 0;
 }
