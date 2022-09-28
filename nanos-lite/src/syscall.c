566080dbeaae417f42bc1232bb79bfb65123cc8b
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-28 22:49:38 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:49:38 up  3:40,  1 user,  load average: 0.31, 0.41, 0.37

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,19 +2,11 @@
 #include "fs.h"
 #include <common.h>
 // #include <stdio.h>
-#define CONFIG_STRACE
 // extern void write_strace(uintptr_t *a);
 // #endif
 
 // extern FILE *strace_fp;
 // extern void write_strace(uintptr_t *a);
-
-// #define IFDEF(macro, ...) #ifdef macro  __VA_ARGS__  #endif
-
-
-
-
-
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
@@ -23,59 +15,49 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
-  // #ifdef CONFIG_STRACE
-  // printf_strace(a);
-  // write_strace(a);
-
-#ifdef CONFIG_STRACE 
   printf("\n[  log  ]system call ! ");
-#endif 
 
-  
-  // #endif
   switch (a[0]) {
   case SYS_exit:
-    printf("a0:%ld, a1:%ld, a2:%lx, a3:%ld  ", a[0], a[1], a[2], a[3]);
+    printf("sys_id:%ld, status:%ld, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
     printf("name : %s", "SYS_exit\n");
     halt(a[1]);
     break;
   case SYS_yield:
-    printf("a0:%ld, a1:%ld, a2:%lx, a3:%ld  ", a[0], a[1], a[2], a[3]);
+    printf("sys_id:%ld, a1:%ld, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
     printf("name : %s", "SYS_yield\n");
     yield();
     c->GPRx = 0;
     break;
   case SYS_open: {
-    printf("a0:%ld, a1:%ld, a2:%lx, a3:%ld  ", a[0], a[1], a[2], a[3]);
+    printf("sys_id:%ld, path:%lx, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
     printf("name : %s", "SYS_open\n");
     c->GPRx = fs_open((char *)a[1], 0, 0);
   } break;
 
   case SYS_read: {
+    printf("sys_id:%ld, fd:%ld, buf:%lx, count:%ld  ", a[0], a[1], a[2], a[3]);
     c->GPRx = fs_read(a[1], (void *)a[2], a[3]);
-    printf("name : %s, data num: %d\n", "SYS_read", c->GPRx);
+    printf("name : %s, transfor data num: %d\n", "SYS_read", c->GPRx);
   } break;
   case SYS_write: {
-    printf("name : %s", "SYS_write\n");
-    // int i = 0;
-    // if (a[1] == 1 || a[1] == 2) {
-    //   for (i = 0; i < a[3]; i++) {
-    //     putch(*((char *)a[2] + i));
-    //   }
-    // }
-    // c->GPRx = i;
+    printf("sys_id:%ld, fd:%ld, buf:%lx, count:%ld  ", a[0], a[1], a[2], a[3]);
     c->GPRx = fs_write(a[1], (void *)a[2], a[3]);
+    printf("name : %s, transfor data num: %d\n", "SYS_write", c->GPRx);
   } break;
 
   case SYS_close: {
+    printf("sys_id:%ld, fd:%ld, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
     printf("name : %s", "SYS_close\n");
     c->GPRx = fs_close(a[1]);
   } break;
   case SYS_lseek: {
+    printf("sys_id:%ld, fd:%ld, offset:%ld, type:%ld  ", a[0], a[1], a[2], a[3]);
     c->GPRx = fs_lseek(a[1], a[2], a[3]);
     printf("name : %s, offset : 0x%x\n", "SYS_lseek", c->GPRx);
   } break;
   case SYS_brk: {
+    printf("sys_id:%ld, increment:%ld, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
     printf("name : %s, malloc : %d\n", "SYS_brk", a[1]);
     malloc(a[1]);
     c->GPRx = 0;
@@ -84,7 +66,7 @@ void do_syscall(Context *c) {
     long int ms = io_read(AM_TIMER_UPTIME).us / 1000;
     *(long int *)a[1] = ms / 1000;
     *((long int *)a[1] + 1) = ms % 1000;
-    // printf("name : %s, get time : %lds %ldms\n", "SYS_gettimeofday",*(long int *)a[1],*((long int *)a[1] + 1));
+    printf("name : %s, get time : %lds %ldms\n", "SYS_gettimeofday",*(long int *)a[1],*((long int *)a[1] + 1));
     c->GPRx = 0;
   } break;
 
