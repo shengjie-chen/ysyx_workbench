acf465e033cd4b7cdc14e051061140e38b20ba4f
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-29 10:53:59 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:53:59 up 19 min,  1 user,  load average: 0.56, 0.56, 0.37

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/types.h>
+// #include <sys/stat.h>
+// #include <sys/time.h>
+// #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
