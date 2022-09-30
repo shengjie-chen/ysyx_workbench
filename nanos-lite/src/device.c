2eb03fe4ab851c2140ab8cf42c32bbda6380fe91
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-30 09:24:10 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:24:10 up  2:25,  1 user,  load average: 0.31, 0.35, 0.35

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -53,6 +53,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   int2char(dis_h, info);
   strcat(pinfo, info);
   printf("%s\n", pinfo);
+  printf("%d\n", pinfo[10]);
   memcpy(buf, pinfo, 25);
   return 0;
 }
