d61da747afc8420f7852a6d5a2975ededf9385e6
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-30 09:31:52 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:31:52 up  2:32,  1 user,  load average: 0.30, 0.27, 0.29

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -44,10 +44,11 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   int dis_w = io_read(AM_GPU_CONFIG).width;
   int dis_h = io_read(AM_GPU_CONFIG).height;
   char pinfo[25]={0};
-  char info[5];
+  char info[5]={0};
   // printf("%d\n", pinfo[0]);
   strcat(pinfo, "WIDTH :");
   int2char(dis_w, info);
+  printf("%s\n", info);
   strcat(pinfo, info);
   memset(info, 0, 5);
   strcat(pinfo, "\nHEIGHT:");
