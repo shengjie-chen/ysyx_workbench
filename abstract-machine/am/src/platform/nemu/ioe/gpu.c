48934006367b2f96e05a656428ec7d77cd7503c7
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-01 22:56:46 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:56:46 up 13:43,  1 user,  load average: 1.31, 0.81, 0.61

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -66,6 +66,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     printf("x=%d,y=%d,w=%d,h=%d\n", x, y, w, h);
     for (i = 0; i < h; i++) {
       for (j = 0; j < w; j++) {
+        printf("4\n");
+
         *(fb + (i + y) * width + j + x) = color_buf[i * w + j];
       }
     }
