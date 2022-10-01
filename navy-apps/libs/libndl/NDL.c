8d9ba2f1673fdf22d66b3550aefdff04b1a8d7c0
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-01 11:20:42 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:20:42 up  6:12,  1 user,  load average: 0.32, 0.26, 0.20

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -118,8 +118,9 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   }
   for (int i = 0; i < h; i++) {
     lseek(fd, x + y * width + i * width, SEEK_SET);
-    write(fd, pixels + w * i, w * 4);
+    write(fd, pixels + w * i, w);
   }
+  close(fd);
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
