a1713d5b4103c2cdb5c34867689a34dd873c2c65
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-01 22:32:53 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:32:53 up 13:19,  1 user,  load average: 0.93, 0.60, 0.50

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -10,8 +10,8 @@ int main() {
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
-  // NDL_DrawRect(bmp, 200-64, 150-64, w, h);
-  NDL_DrawRect(bmp, 0, 0, w, h);
+  NDL_DrawRect(bmp, 200-64, 150-64, w, h);
+  // NDL_DrawRect(bmp, 0, 0, w, h);
   free(bmp);
   NDL_Quit();
   printf("Test ends! Spinning...\n");
