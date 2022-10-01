dfe0ff153c22ada2a8b791f7a65b157da4526b97
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-01 15:25:58 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:25:58 up  8:19,  1 user,  load average: 0.24, 0.23, 0.18

---
+++ navy-apps/tests/fixedptc-test/main.c
@@ -0,0 +1,43 @@
+#include <BMP.h>
+#include <NDL.h>
+#include <assert.h>
+#include <fixedptc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  fixedpt A = fixedpt_rconst(1.2);
+  fixedpt B = fixedpt_fromint(10);
+  fixedpt C = 0;
+  if (B > fixedpt_rconst(7.9)) {
+    C = fixedpt_div(fixedpt_mul(A + FIXEDPT_ONE, B), fixedpt_rconst(2.3));
+  }
+
+  float a = 1.2;
+  float b = 10;
+  int c = 0;
+  if (b > 7.9) {
+    c = (a + 1) * b / 2.3;
+  }
+
+  if (c != fixedpt_toint(C)) {
+    printf("fixedpt function error!\n");
+  }
+
+  // srand((unsigned int)time(NULL));
+
+  //   for(int i=0;i < 100;i++){
+  // int u_float = rand() % 1000 ;
+  // int u_int = rand() % 1000;
+  // int u_sign = rand() % 2;
+  // int spec_floor;
+  // int spec_ceil;
+
+  //   }
+  // printf("fixedpt floor function pass!\n");
+  printf("1.2 floor result is %d\n", fixedpt_toint(fixedpt_floor(fixedpt_rconst(1.2))));
+  printf("1.2 ceil result is %d\n", fixedpt_toint(fixedpt_ceil(fixedpt_rconst(1.2))));
+
+  return 0;
+}
