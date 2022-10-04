97b3aae316b8d4f666f68828233a3f0f51003699
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-04 22:26:05 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:26:05 up  6:24,  1 user,  load average: 0.30, 0.52, 0.53

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -7,26 +7,33 @@ static const char *keyname[] = {
     "NONE",
     _KEYS(keyname)};
 
+static int SDL_ConvertEvent(char *key) {
+  // printf("key:%s\n", key);
+  // printf("key size:%d\n", sizeof(keyname) );
+  // printf("key num:%d\n", sizeof(keyname) / sizeof(keyname[0]));
+  for (int i = 0; i < sizeof(keyname) / sizeof(keyname[0]); i++) {
+    if (strcmp(key, keyname[i]) == 0) {
+      return i;
+    }
+  }
+  return 0;
+}
+
 int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
-}
-
-#define strdef(k) #k,
-
-const char *key_str[] = {"NONE", _KEYS(strdef)};
-
-static int SDL_ConvertEvent(char *key) {
-  // printf("key:%s\n", key);
-  // printf("key size:%d\n", sizeof(key_str) );
-  // printf("key num:%d\n", sizeof(key_str) / sizeof(key_str[0]));
-  for (int i = 0; i < sizeof(key_str) / sizeof(key_str[0]); i++) {
-    if (strcmp(key, key_str[i]) == 0) {
-      return i;
-    }
+  char buf[64];
+  if (NDL_PollEvent(buf, sizeof(buf))) {
+    buf[strlen(buf) - 1] = 0;
+    // printf("receive event: %s\n", buf);
+    // printf("%c\n", buf[1]);
+    ev->type = (buf[1] == 'd') ? SDL_KEYDOWN : SDL_KEYUP;
+    // printf("SDL_EventType: %d\n", ev->type);
+    ev->key.keysym.sym = SDL_ConvertEvent(&buf[3]);
+    // printf("SDL_Keys: %d\n", ev->key.keysym.sym);
+    return 1;
   }
   return 0;
 }
@@ -45,18 +52,6 @@ int SDL_WaitEvent(SDL_Event *event) {
       return 1;
     }
   }
-  // if (NDL_PollEvent(buf, sizeof(buf))) {
-  //   buf[strlen(buf) - 1] = 0;
-  //   printf("receive event: %s\n", buf);
-  //   // printf("%c\n", buf[1]);
-  //   event->type = (buf[1] == 'd') ? SDL_KEYDOWN : SDL_KEYUP;
-  //   printf("SDL_EventType: %d\n", event->type);
-  //   event->key.keysym.sym = SDL_ConvertEvent(&buf[3]);
-  //   printf("SDL_Keys: %d\n", event->key.keysym.sym);
-  // } else {
-  //   event->type = SDL_USEREVENT;
-  // }
-  // return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
