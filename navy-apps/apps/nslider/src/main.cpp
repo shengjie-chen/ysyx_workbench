1686246c7eac125c4326be88ce2faac614e5971a
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-04 17:38:48 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:38:48 up  3:22,  1 user,  load average: 0.62, 0.58, 0.50

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -1,7 +1,7 @@
 #include <SDL.h>
 #include <SDL_bmp.h>
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 
 #define W 400
 #define H 300
@@ -20,6 +20,8 @@ static SDL_Surface *slide = NULL;
 static int cur = 0;
 
 void render() {
+  printf("1\n");
+
   if (slide) {
     SDL_FreeSurface(slide);
   }
@@ -31,16 +33,20 @@ void render() {
 }
 
 void prev(int rep) {
-  if (rep == 0) rep = 1;
+  if (rep == 0)
+    rep = 1;
   cur -= rep;
-  if (cur < 0) cur = 0;
+  if (cur < 0)
+    cur = 0;
   render();
 }
 
 void next(int rep) {
-  if (rep == 0) rep = 1;
+  if (rep == 0)
+    rep = 1;
   cur += rep;
-  if (cur >= N) cur = N - 1;
+  if (cur >= N)
+    cur = N - 1;
   render();
 }
 
@@ -55,30 +61,59 @@ int main() {
   while (1) {
     SDL_Event e;
     SDL_WaitEvent(&e);
-    
+
     if (e.type == SDL_KEYDOWN) {
-      switch(e.key.keysym.sym) {
-        case SDLK_0: rep = rep * 10 + 0; break;
-        case SDLK_1: rep = rep * 10 + 1; break;
-        case SDLK_2: rep = rep * 10 + 2; break;
-        case SDLK_3: rep = rep * 10 + 3; break;
-        case SDLK_4: rep = rep * 10 + 4; break;
-        case SDLK_5: rep = rep * 10 + 5; break;
-        case SDLK_6: rep = rep * 10 + 6; break;
-        case SDLK_7: rep = rep * 10 + 7; break;
-        case SDLK_8: rep = rep * 10 + 8; break;
-        case SDLK_9: rep = rep * 10 + 9; break;
-        case SDLK_J:
-        case SDLK_DOWN: next(rep); rep = 0; g = 0; break;
-        case SDLK_K:
-        case SDLK_UP: prev(rep); rep = 0; g = 0; break;
-        case SDLK_G:
-          g ++;
-          if (g > 1) {
-            prev(100000);
-            rep = 0; g = 0;
-          }
-          break;
+      switch (e.key.keysym.sym) {
+      case SDLK_0:
+        rep = rep * 10 + 0;
+        break;
+      case SDLK_1:
+        rep = rep * 10 + 1;
+        break;
+      case SDLK_2:
+        rep = rep * 10 + 2;
+        break;
+      case SDLK_3:
+        rep = rep * 10 + 3;
+        break;
+      case SDLK_4:
+        rep = rep * 10 + 4;
+        break;
+      case SDLK_5:
+        rep = rep * 10 + 5;
+        break;
+      case SDLK_6:
+        rep = rep * 10 + 6;
+        break;
+      case SDLK_7:
+        rep = rep * 10 + 7;
+        break;
+      case SDLK_8:
+        rep = rep * 10 + 8;
+        break;
+      case SDLK_9:
+        rep = rep * 10 + 9;
+        break;
+      case SDLK_J:
+      case SDLK_DOWN:
+        next(rep);
+        rep = 0;
+        g = 0;
+        break;
+      case SDLK_K:
+      case SDLK_UP:
+        prev(rep);
+        rep = 0;
+        g = 0;
+        break;
+      case SDLK_G:
+        g++;
+        if (g > 1) {
+          prev(100000);
+          rep = 0;
+          g = 0;
+        }
+        break;
       }
     }
   }
