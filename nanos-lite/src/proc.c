ca2d938429f32a900d04f117fd1e670cbd40233b
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-07 18:56:19 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:56:19 up 13 min,  1 user,  load average: 0.83, 0.66, 0.38

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,7 +29,7 @@ void init_proc() {
   // load program here
   // naive_uload(NULL, NULL);
 
-  naive_uload(NULL,"/bin/menu");
+  // naive_uload(NULL,"/bin/menu");
   // naive_uload(NULL,"/bin/nterm");
   // naive_uload(NULL,"/bin/nslider");
   // naive_uload(NULL,"/bin/bird");
@@ -38,7 +38,7 @@ void init_proc() {
   // naive_uload(NULL,"/bin/fixedptc-test");
   // naive_uload(NULL,"/bin/event-test");
   // naive_uload(NULL,"/bin/bmp-test");
-  // naive_uload(NULL,"/bin/timer-test");
+  naive_uload(NULL,"/bin/timer-test");
   // naive_uload(NULL,"/bin/file-test");
   // naive_uload(NULL,"/bin/hello");
 }
