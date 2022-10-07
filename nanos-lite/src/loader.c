59b2d64bb76535c7e38aa3a6ed72ad81e71daf80
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-07 19:44:50 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:44:50 up  1:02,  1 user,  load average: 1.05, 0.57, 0.42

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -27,34 +27,20 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr elf_head;
 
   // 读取 head 到elf_head
-  // ramdisk_read(&elf_head, 0, sizeof(Elf_Ehdr));
   int fd = fs_open(filename, 0, 0);
   fs_read(fd, &elf_head, sizeof(Elf_Ehdr));
 
   // 判断elf文件类型 machine
   assert(*(uint32_t *)elf_head.e_ident == 0x464c457f);
-  // printf("%d\n",elf_head.e_machine);
   assert(elf_head.e_machine == EXPECT_TYPE);
 
   uintptr_t entry = 0;
   Elf_Phdr elf_seg;
 
-  // uintptr_t vaddr_offset = elf_head.e_phoff + elf_head.e_phnum * elf_head.e_phentsize;
-  // for (int i = 0; i < elf_head.e_phnum; i++) {
-  //   ramdisk_read(&elf_seg, elf_head.e_phoff + i * elf_head.e_phentsize, sizeof(Elf_Phdr));
-  //   if (elf_seg.p_type == PT_LOAD) {
-  //     memcpy((void *)elf_seg.p_vaddr + vaddr_offset, elf_head.e_phoff + elf_head.e_phnum * elf_head.e_phentsize + (&ramdisk_start), elf_seg.p_filesz);
-  //     memset((void *)(elf_seg.p_vaddr + vaddr_offset + elf_seg.p_filesz), 0, elf_seg.p_memsz - elf_seg.p_filesz);
-  //   }
-  // }
-
   for (int i = 0; i < elf_head.e_phnum; i++) {
-    // ramdisk_read(&elf_seg, elf_head.e_phoff + i * elf_head.e_phentsize, sizeof(Elf_Phdr));
     fs_lseek(fd, elf_head.e_phoff + i * elf_head.e_phentsize, SEEK_SET);
     fs_read(fd, &elf_seg, sizeof(Elf_Phdr));
     if (elf_seg.p_type == PT_LOAD) {
-      // memcpy((void *)elf_seg.p_vaddr, elf_seg.p_offset + (&ramdisk_start), elf_seg.p_filesz);
-      // memset((void *)(elf_seg.p_vaddr + elf_seg.p_filesz), 0, elf_seg.p_memsz - elf_seg.p_filesz);
       fs_lseek(fd, elf_seg.p_offset, SEEK_SET);
       fs_read(fd, (void *)elf_seg.p_vaddr, elf_seg.p_filesz);
       memset((void *)(elf_seg.p_vaddr + elf_seg.p_filesz), 0, elf_seg.p_memsz - elf_seg.p_filesz);
@@ -62,7 +48,6 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   }
   fs_close(fd);
   entry = elf_head.e_entry;
-  // printf("%x\n", entry);
   return entry;
 }
 
