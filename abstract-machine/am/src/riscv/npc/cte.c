6fe0c215b24329b853ff2b311360c3deb3b83835
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-13 17:07:40 +0800

    >  sim RVNoob RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:07:40 up 12 min,  1 user,  load average: 0.36, 0.76, 0.66

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -1,27 +1,39 @@
 #include <am.h>
 #include <klib.h>
 
-static Context* (*user_handler)(Event, Context*) = NULL;
+static Context *(*user_handler)(Event, Context *) = NULL;
 
-Context* __am_irq_handle(Context *c) {
+Context *__am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      default: ev.event = EVENT_ERROR; break;
+    case 11: {
+      register intptr_t type asm("a7"); // or use c->GPR1
+      if (type == -1) {
+        c->mepc += 4;
+        ev.event = EVENT_YIELD;
+      } else {
+        c->mepc += 4;
+        ev.event = EVENT_SYSCALL;
+      }
+    } break;
+    default:
+      ev.event = EVENT_ERROR;
+      break;
     }
-
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-
   return c;
 }
 
 extern void __am_asm_trap(void);
 
-bool cte_init(Context*(*handler)(Event, Context*)) {
+bool cte_init(Context *(*handler)(Event, Context *)) {
   // initialize exception entry
-  asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
+  asm volatile("csrw mtvec, %0"
+               :
+               : "r"(__am_asm_trap));
 
   // register event handler
   user_handler = handler;
