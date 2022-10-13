d1ed7bcc4a629da18171491fcf96d8c79f03a189
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-13 20:40:03 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:40:03 up 28 min,  1 user,  load average: 0.41, 0.31, 0.30

--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -1,6 +1,10 @@
 #include <am.h>
+#include <npc.h>
+
+#define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t kbd_msg = inl(KBD_ADDR);
+  kbd->keydown = (kbd_msg & KEYDOWN_MASK) ? true : false;
+  kbd->keycode = kbd_msg & ~KEYDOWN_MASK;
 }
