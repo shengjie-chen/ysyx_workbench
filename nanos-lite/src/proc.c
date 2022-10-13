4399934889692a59c38a917ed6ef1dfed65730de
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-13 14:38:15 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:38:15 up  1:10,  1 user,  load average: 0.15, 0.18, 0.31

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,10 +31,10 @@ void init_proc() {
 
   // APPS
   // naive_uload(NULL,"/bin/nslider");
-  // naive_uload(NULL,"/bin/menu");
+  naive_uload(NULL,"/bin/menu");
   // naive_uload(NULL,"/bin/nterm");
   // naive_uload(NULL,"/bin/bird");
-  naive_uload(NULL, "/bin/pal");
+  // naive_uload(NULL, "/bin/pal");
 
   // TESTS
   // naive_uload(NULL,"/bin/dummy");
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,7 +1,10 @@
 #include "syscall.h"
 #include "fs.h"
 #include <common.h>
+#include <proc.h>
+
 // ctrl + H : uprintf -> //uprintf
+extern void naive_uload(PCB *pcb, const char *filename);
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
@@ -10,24 +13,25 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
-  //printf("\n[  log  ]system call ! ");
+  // printf("\n[  log  ]system call ! ");
 
   switch (a[0]) {
   case SYS_exit:
-    //printf("sys_id:%ld, status:%ld, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
-    //printf("name : %s", "SYS_exit\n");
-    halt(a[1]);
+    // printf("sys_id:%ld, status:%ld, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
+    // printf("name : %s", "SYS_exit\n");
+    // halt(a[1]);
+    naive_uload(NULL,"/bin/menu");
     break;
   case SYS_yield:
-    //printf("sys_id:%ld, a1:%ld, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
-    //printf("name : %s", "SYS_yield\n");
+    // printf("sys_id:%ld, a1:%ld, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
+    // printf("name : %s", "SYS_yield\n");
     yield();
     c->GPRx = 0;
     break;
   case SYS_open: {
-    //printf("sys_id:%ld, path:%lx, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
+    // printf("sys_id:%ld, path:%lx, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
     c->GPRx = fs_open((char *)a[1], 0, 0);
-    //printf("name : %s, return fd : %d\n", "SYS_open", c->GPRx);
+    // printf("name : %s, return fd : %d\n", "SYS_open", c->GPRx);
   } break;
 
   case SYS_read: {
@@ -36,34 +40,38 @@ void do_syscall(Context *c) {
     // printf("name : %s, transfor data num: %d\n", "SYS_read", c->GPRx);
   } break;
   case SYS_write: {
-    //printf("sys_id:%ld, fd:%ld, buf:%lx, count:%ld  ", a[0], a[1], a[2], a[3]);
-    //printf("name : %s, wirte usage:\n", "SYS_write");
+    // printf("sys_id:%ld, fd:%ld, buf:%lx, count:%ld  ", a[0], a[1], a[2], a[3]);
+    // printf("name : %s, wirte usage:\n", "SYS_write");
     c->GPRx = fs_write(a[1], (void *)a[2], a[3]);
   } break;
 
   case SYS_close: {
-    //printf("sys_id:%ld, fd:%ld, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
-    //printf("name : %s", "SYS_close\n");
+    // printf("sys_id:%ld, fd:%ld, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
+    // printf("name : %s", "SYS_close\n");
     c->GPRx = fs_close(a[1]);
   } break;
   case SYS_lseek: {
-    //printf("sys_id:%ld, fd:%ld, offset:%ld, type:%ld  ", a[0], a[1], a[2], a[3]);
+    // printf("sys_id:%ld, fd:%ld, offset:%ld, type:%ld  ", a[0], a[1], a[2], a[3]);
     c->GPRx = fs_lseek(a[1], a[2], a[3]);
-    //printf("name : %s, offset : 0x%x\n", "SYS_lseek", c->GPRx);
+    // printf("name : %s, offset : 0x%x\n", "SYS_lseek", c->GPRx);
   } break;
   case SYS_brk: {
-    //printf("sys_id:%ld, increment:%ld, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
-    //printf("name : %s, malloc : %d\n", "SYS_brk", a[1]);
+    // printf("sys_id:%ld, increment:%ld, a2:%ld, a3:%ld  ", a[0], a[1], a[2], a[3]);
+    // printf("name : %s, malloc : %d\n", "SYS_brk", a[1]);
     malloc(a[1]);
     c->GPRx = 0;
   } break;
   case SYS_gettimeofday: {
-    long int us = io_read(AM_TIMER_UPTIME).us ;
+    long int us = io_read(AM_TIMER_UPTIME).us;
     *(long int *)a[1] = us / 1000000;
     *((long int *)a[1] + 1) = us % 1000000;
     // printf("name : %s, get time : %lds %ldus\n", "SYS_gettimeofday", *(long int *)a[1], *((long int *)a[1] + 1));
     c->GPRx = 0;
   } break;
+  case SYS_execve: {
+    naive_uload(NULL, (const char *)a[1]);
+    c->GPRx = -1;
+  }
 
   default:
     panic("Unhandled syscall ID = %d", a[0]);
