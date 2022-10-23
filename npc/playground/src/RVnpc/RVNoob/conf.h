c2f9ce6af1d1196e8a5d4c0efbe2f74e300f45a0
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-23 15:20:34 +0800

    >  sim RVNoob RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:20:34 up 8 min,  1 user,  load average: 0.67, 0.95, 0.65

--- npc/playground/src/RVnpc/RVNoob/conf.h
+++ npc/playground/src/RVnpc/RVNoob/conf.h
@@ -1,40 +1,52 @@
 #ifndef __MACRO_CONF__
 #define __MACRO_CONF__
 
-// switch
-#define CONFIG_VGA_SHOW_SCREEN 1
-#define CONFIG_VGA_SIZE_400x300 1
+// ---------------------------->switch
 
-#define BIGPROGRAM
-
-#ifdef BIGPROGRAM
-#define BIGPROGRAMDEBUG
-#define CONFIG_DIFFTEST
-#endif
-
-// switch end
-
-#ifndef BIGPROGRAM
 #define CONFIG_ITRACE
 #define CONFIG_FTRACE
-#define CONFIG_MTRACE
-#define CONFIG_DUMPVCD
-#define CONFIG_DIFFTEST
-#define CONFIG_DUMPSTART 0
-#endif
-
-#ifdef BIGPROGRAMDEBUG
 // #define CONFIG_MTRACE
-#define CONFIG_ITRACE
-#define CONFIG_FTRACE
-// 1M --> 1.6GB
-#define CONFIG_DUMPVCD
-#define CONFIG_DUMPSTART 0
-// #define CONFIG_DUMPSTART 11200000
-#endif
+
+#define CONFIG_DIFFTEST
+
+// #define CONFIG_DUMPVCD
+// #define CONFIG_DUMPSTART 0
+
+// ---------------------------->switch end
+
+
+// #define BIGPROGRAM
+
+// #ifdef BIGPROGRAM
+// #define BIGPROGRAMDEBUG
+// #define CONFIG_DIFFTEST
+// #endif
+
+
+// #ifndef BIGPROGRAM
+// #define CONFIG_ITRACE
+// #define CONFIG_FTRACE
+// #define CONFIG_MTRACE
+// #define CONFIG_DUMPVCD
+// #define CONFIG_DIFFTEST
+// #define CONFIG_DUMPSTART 0
+// #endif
+
+// #ifdef BIGPROGRAMDEBUG
+// // #define CONFIG_MTRACE
+// #define CONFIG_ITRACE
+// #define CONFIG_FTRACE
+// // 1M --> 1.6GB
+// #define CONFIG_DUMPVCD
+// #define CONFIG_DUMPSTART 0
+// // #define CONFIG_DUMPSTART 11200000
+// #endif
 
 #define CONFIG_ISA64 1
 #define CONFIG_MSIZE 0x8000000
 #define CONFIG_MBASE 0x80000000
+
+#define CONFIG_VGA_SHOW_SCREEN 1
+#define CONFIG_VGA_SIZE_400x300 1
 
 #endif
