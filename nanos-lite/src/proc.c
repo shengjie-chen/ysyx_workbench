a073126071bec90fe0d819f701636d80a8bcbd64
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-23 16:32:19 +0800

    >  sim RVNoob RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:32:19 up  1:19,  1 user,  load average: 0.91, 1.05, 1.20

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -33,8 +33,8 @@ void init_proc() {
   // naive_uload(NULL,"/bin/nslider");
   // naive_uload(NULL,"/bin/menu");
   // naive_uload(NULL,"/bin/nterm");
-  // naive_uload(NULL,"/bin/bird");
-  naive_uload(NULL, "/bin/pal");
+  naive_uload(NULL,"/bin/bird");
+  // naive_uload(NULL, "/bin/pal");
 
   // TESTS
   // naive_uload(NULL,"/bin/dummy");
