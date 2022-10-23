bcf6e2affec80c8e60e67cd79b29b3049fffdde4
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-23 15:34:28 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:34:28 up 21 min,  1 user,  load average: 1.11, 0.69, 0.61

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -65,6 +65,7 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count) {
   int status;
+  printf("_write, fd:%ld, buf:%lx, count:%ld  \n", fd, buf, count);
   status = _syscall_(SYS_write, fd, (intptr_t)buf, count);
   if (status == -1 || status > count) {
     _exit(1);
@@ -92,7 +93,6 @@ void *_sbrk(intptr_t increment) {
 }
 
 int _read(int fd, void *buf, size_t count) {
-  printf("_read, fd:%ld, buf:%lx, count:%ld  \n", fd, buf, count);
   return _syscall_(SYS_read, fd, buf, count);
   // return 0;
 }
