8fa14601320b6fdf7751558bfed9cbc421fa998d
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-26 22:17:05 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:17:05 up  1:05,  1 user,  load average: 14.58, 6.03, 2.61

--- npc/playground/src/RVnpc/RVNoob/CSR.scala
+++ npc/playground/src/RVnpc/RVNoob/CSR.scala
@@ -17,7 +17,7 @@ class CSR extends Module with RVNoobConfig with Csr_op {
   })
   // 0 mstatus; 1 mtvec; 2 mepc; 3 mcause;
   //  val csr = Reg(Vec(4, UInt(xlen.W)))
-  val csr = RegInit(Vec(4, UInt(xlen.W)),VecInit(0x80000000L.U,0.U,0.U,0.U))
+  val csr = RegInit(Vec(4, UInt(xlen.W)),VecInit(0xa00001800L.U,0.U,0.U,0.U))
   val csr_raddr = MuxCase(
     0.U,
     Array(
