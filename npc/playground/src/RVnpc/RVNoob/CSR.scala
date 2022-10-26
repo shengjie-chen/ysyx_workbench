1f58b35b8ab34f4193248a121049d64b8cef52ff
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-26 22:15:47 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:15:47 up  1:04,  1 user,  load average: 1.41, 0.73, 0.69

--- npc/playground/src/RVnpc/RVNoob/CSR.scala
+++ npc/playground/src/RVnpc/RVNoob/CSR.scala
@@ -5,18 +5,19 @@ import chisel3.util._
 
 class CSR extends Module with RVNoobConfig with Csr_op {
   val io = IO(new Bundle {
-    val csr_rdata = Output(UInt(xlen.W))
-    val id_csr_ctrl   = Input(new IdCsrCtrlIO)
-    val csr_dnpc  = Output(UInt(xlen.W))
+    val csr_rdata   = Output(UInt(xlen.W))
+    val id_csr_ctrl = Input(new IdCsrCtrlIO)
+    val csr_dnpc    = Output(UInt(xlen.W))
 
-    val pc         = Input(UInt(xlen.W))
-    val mcause     = Input(UInt(xlen.W))
+    val pc          = Input(UInt(xlen.W))
+    val mcause      = Input(UInt(xlen.W))
     val wb_csr_ctrl = Input(new WbCsrCtrlIO)
-    val csr_wdata  = Input(UInt(xlen.W))
+    val csr_wdata   = Input(UInt(xlen.W))
 
   })
   // 0 mstatus; 1 mtvec; 2 mepc; 3 mcause;
-  val csr = Reg(Vec(4, UInt(xlen.W)))
+  //  val csr = Reg(Vec(4, UInt(xlen.W)))
+  val csr = RegInit(Vec(4, UInt(xlen.W)),VecInit(0x80000000L.U,0.U,0.U,0.U))
   val csr_raddr = MuxCase(
     0.U,
     Array(
@@ -27,7 +28,7 @@ class CSR extends Module with RVNoobConfig with Csr_op {
     )
   )
   io.csr_rdata := Mux(io.id_csr_ctrl.csr_ren, csr(csr_raddr), 0.U)
-  io.csr_dnpc := Mux(io.id_csr_ctrl.ecall,csr(1),csr(2))  // this 2 signal can mix
+  io.csr_dnpc  := Mux(io.id_csr_ctrl.ecall, csr(1), csr(2)) // this 2 signal can mix
 
   val csr_waddr = MuxCase(
     0.U,
@@ -38,9 +39,9 @@ class CSR extends Module with RVNoobConfig with Csr_op {
       (io.wb_csr_ctrl.csr_waddr === 0x342.U) -> 3.U
     )
   )
-  when(io.wb_csr_ctrl.csr_wen){
+  when(io.wb_csr_ctrl.csr_wen) {
     csr(csr_waddr) := io.csr_wdata
-  }.elsewhen(io.wb_csr_ctrl.ecall){
+  }.elsewhen(io.wb_csr_ctrl.ecall) {
     csr(2) := io.pc
     csr(3) := io.mcause
   }
