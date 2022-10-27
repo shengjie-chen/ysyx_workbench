8fd14be1c77929bd8ad3768c8bf4468ff2cbb336
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-27 11:08:54 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:08:54 up  1:05,  1 user,  load average: 0.58, 0.48, 0.44

--- npc/playground/src/RVnpc/RVNoob/RVNoob.h
+++ npc/playground/src/RVnpc/RVNoob/RVNoob.h
@@ -54,13 +54,13 @@ const uint32_t img[] = {
     0x00840413, // addi	s0,s0,8
     0x004a0a13, // addi	s4,s4,4
     // 0x00b13423, // sd	a1,8(sp)
-    // 0x00006597, // auipc	a1,0x6
-    // 0xff010113, // addi	sp,sp,-16
+    0x00006597, // auipc	a1,0x6
+    0xff010113, // addi	sp,sp,-16
     // 0x00813983, // ld	s3,8(sp)  
-    // 0x00000517, // auipc	a0,0x0
-    // 0x0015869b, // addiw	a3,a1,1
-    // 0x01c50513, // addi	a0,a0,28 
-    // 0x00050513, // mv	a0,a0
+    0x00000517, // auipc	a0,0x0
+    0x0015869b, // addiw	a3,a1,1
+    0x01c50513, // addi	a0,a0,28 
+    0x00050513, // mv	a0,a0
     0x00100073  // ebreak
     //  0x0102b503,  // ld  a0,16(t0)
     //  0x00100073,  // ebreak (used as NPC_trap)
