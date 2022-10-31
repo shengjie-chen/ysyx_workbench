503b9f305fe3d72804a421b5cb3478fe04021b78
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-31 18:07:29 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:07:29 up  4:44,  1 user,  load average: 0.43, 0.41, 0.40

--- npc/playground/src/RVnpc/RVNoob/CSR.scala
+++ npc/playground/src/RVnpc/RVNoob/CSR.scala
@@ -4,7 +4,7 @@ import chisel3._
 import chisel3.util._
 import scala.math._
 
-class CSR_read_dpi extends BlackBox with RVNoobConfig{
+class CSR_read_dpi extends BlackBox with RVNoobConfig {
   val io = IO(new Bundle {
     val csr = Input(Vec(4, UInt(xlen.W)))
   })
@@ -12,33 +12,24 @@ class CSR_read_dpi extends BlackBox with RVNoobConfig{
 
 class CSR extends Module with RVNoobConfig with Csr_op {
   val io = IO(new Bundle {
-    val csr_rdata   = Output(UInt(xlen.W))
+    val csr_rdata = Output(UInt(xlen.W))
     val id_csr_ctrl = Input(new IdCsrCtrlIO)
-    val csr_dnpc    = Output(UInt(xlen.W))
+    val csr_dnpc = Output(UInt(xlen.W))
 
-    val pc          = Input(UInt(xlen.W))
-    val mcause      = Input(UInt(xlen.W))
+    val pc = Input(UInt(xlen.W))
+    val mcause = Input(UInt(xlen.W))
     val wb_csr_ctrl = Input(new WbCsrCtrlIO)
-    val csr_wdata   = Input(UInt(xlen.W))
+    val csr_wdata = Input(UInt(xlen.W))
 
   })
   // 0 mstatus; 1 mtvec; 2 mepc; 3 mcause;
   val csr = Reg(Vec(4, UInt(xlen.W)))
-//  val csr = RegInit(Vec(4, UInt(xlen.W)),VecInit(0xa00001800L.U,0.U,0.U,0.U))
+  val csr_read = Wire(Vec(4, UInt(xlen.W)))
+  csr_read := csr
+
+  //  val csr = RegInit(Vec(4, UInt(xlen.W)),VecInit(0xa00001800L.U,0.U,0.U,0.U))
   val csr_read_dpi = Module(new CSR_read_dpi)
   csr_read_dpi.io.csr <> csr
-
-  val csr_raddr = MuxCase(
-    0.U,
-    Array(
-      (io.id_csr_ctrl.csr_raddr === 0x300.U) -> 0.U, // mstatus
-      (io.id_csr_ctrl.csr_raddr === 0x305.U) -> 1.U, // mtvec
-      (io.id_csr_ctrl.csr_raddr === 0x341.U) -> 2.U, // mepc
-      (io.id_csr_ctrl.csr_raddr === 0x342.U) -> 3.U  // mcause
-    )
-  )
-  io.csr_rdata := Mux(io.id_csr_ctrl.csr_ren, csr(csr_raddr), 0.U)
-  io.csr_dnpc  := Mux(io.id_csr_ctrl.ecall, csr(1), csr(2)) // this 2 signal can mix
 
   val csr_waddr = MuxCase(
     0.U,
@@ -51,8 +42,23 @@ class CSR extends Module with RVNoobConfig with Csr_op {
   )
   when(io.wb_csr_ctrl.csr_wen) {
     csr(csr_waddr) := io.csr_wdata
+    csr_read(csr_waddr) := io.csr_wdata
   }.elsewhen(io.wb_csr_ctrl.ecall) {
     csr(2) := io.pc
     csr(3) := io.mcause
+    csr_read(2) := io.pc
+    csr_read(3) := io.mcause
   }
+
+  val csr_raddr = MuxCase(
+    0.U,
+    Array(
+      (io.id_csr_ctrl.csr_raddr === 0x300.U) -> 0.U, // mstatus
+      (io.id_csr_ctrl.csr_raddr === 0x305.U) -> 1.U, // mtvec
+      (io.id_csr_ctrl.csr_raddr === 0x341.U) -> 2.U, // mepc
+      (io.id_csr_ctrl.csr_raddr === 0x342.U) -> 3.U // mcause
+    )
+  )
+  io.csr_rdata := Mux(io.id_csr_ctrl.csr_ren, csr_read(csr_raddr), 0.U)
+  io.csr_dnpc := Mux(io.id_csr_ctrl.ecall, csr_read(1), csr_read(2)) // this 2 signal can mix
 }
