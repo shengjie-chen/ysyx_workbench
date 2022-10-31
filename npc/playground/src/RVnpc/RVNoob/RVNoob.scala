c7fdfba787122b9709ae64834a11588d1d2fce88
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-31 16:22:08 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:22:08 up  2:59,  1 user,  load average: 1.44, 1.03, 0.81

--- npc/playground/src/RVnpc/RVNoob/RVNoob.scala
+++ npc/playground/src/RVnpc/RVNoob/RVNoob.scala
@@ -12,33 +12,40 @@ class RVNoob extends Module with ext_function {
     //    override val prefix
   })
 
-  val pipelineBypass : Boolean = false
+  val pipelineBypass: Boolean = false
 
   // ********************************** Instance **********************************
   // >>>>>>>>>>>>>> IF inst Fetch <<<<<<<<<<<<<<
   val dnpc_en = Wire(Bool())
-  val flush = Wire(Bool())
-  val npc  = Wire(UInt(64.W))
-  val pc   = RegEnable(npc,0x80000000L.U(64.W),hazard.io.pc_en) //2147483648
-  val snpc = Wire(UInt(64.W))
+  val flush   = Wire(Bool())
+  val npc     = Wire(UInt(64.W))
+  val pc      = RegEnable(npc, 0x80000000L.U(64.W), hazard.io.pc_en) //2147483648
+  val snpc    = Wire(UInt(64.W))
 
   // >>>>>>>>>>>>>> ID Inst Decode  id_reg <<<<<<<<<<<<<<
-  val id_reg = IDreg(pc, io.inst, snpc,hazard.io.id_reg_ctrl.en,pipelineBypass)
+  val id_reg = IDreg(pc, io.inst, snpc, hazard.io.id_reg_ctrl.en, pipelineBypass)
   val idu    = Module(new IDU)
   val rf     = Module(new RegisterFile)
   val csr    = Module(new CSR)
-  val hazard : HazardDetect = HazardDetect(idu.io.id_rf_ctrl,idu.io.id_csr_ctrl,ex_reg.out.wb_rf_ctrl,ex_reg.out.wb_csr_ctrl,mem_reg.out.wb_rf_ctrl,mem_reg.out.wb_csr_ctrl,dnpc_en)
-
+  val hazard: HazardDetect = HazardDetect(
+    idu.io.id_rf_ctrl,
+    idu.io.id_csr_ctrl,
+    ex_reg.out.wb_rf_ctrl,
+    ex_reg.out.wb_csr_ctrl,
+    mem_reg.out.wb_rf_ctrl,
+    mem_reg.out.wb_csr_ctrl,
+    dnpc_en
+  )
 
   // >>>>>>>>>>>>>> EXE ex_reg <<<<<<<<<<<<<<
   val dnpc_t      = Wire(UInt(64.W))
   val npc_add_res = Wire(UInt(64.W))
-  val ex_reg = EXreg(
+  val ex_reg: EXreg = EXreg(
     id_reg.out.pc,
     id_reg.out.inst,
     id_reg.out.snpc,
-    Mux(idu.io.id_csr_ctrl.zimm_en,uext_64(idu.io.id_rf_ctrl.rs1),rf.io.rdata1),
-    Mux(idu.io.id_csr_ctrl.csr_ren,csr.io.csr_rdata,rf.io.rdata2),
+    Mux(idu.io.id_csr_ctrl.zimm_en, uext_64(idu.io.id_rf_ctrl.rs1), rf.io.rdata1),
+    Mux(idu.io.id_csr_ctrl.csr_ren, csr.io.csr_rdata, rf.io.rdata2),
     idu.io.imm,
     csr.io.csr_dnpc,
     idu.io.exe_ctrl,
@@ -52,7 +59,7 @@ class RVNoob extends Module with ext_function {
   val exe = Module(new EXE)
 
   // >>>>>>>>>>>>>> MEM mem_reg <<<<<<<<<<<<<<
-  val mem_reg = MEMreg(
+  val mem_reg: MEMreg = MEMreg(
     ex_reg.out.pc,
     ex_reg.out.inst,
     dnpc_t,
@@ -86,12 +93,12 @@ class RVNoob extends Module with ext_function {
 
   // ********************************** Connect and Logic **********************************
   // >>>>>>>>>>>>>> IF inst Fetch <<<<<<<<<<<<<<
-  snpc  := pc + 4.U
+  snpc    := pc + 4.U
   dnpc_en := mem_reg.out.pc_mux || mem_reg.out.B_en
-  flush := dnpc_en
-  npc   := Mux(dnpc_en, mem_reg.out.dnpc, snpc)
-  pc    := npc
-  io.pc := pc
+  flush   := dnpc_en
+  npc     := Mux(dnpc_en, mem_reg.out.dnpc, snpc)
+  pc      := npc
+  io.pc   := pc
   val dpi_npc = Module(new DpiNpc) // use to get npc in sim.c
   dpi_npc.io.npc <> npc
 
@@ -141,11 +148,11 @@ class RVNoob extends Module with ext_function {
 
   // >>>>>>>>>>>>>> WB wb_reg <<<<<<<<<<<<<<
   wb_reg.reset <> hazard.io.wb_reg_ctrl.flush
-  rf.io.wdata <> Mux(wb_reg.out.wb_csr_ctrl.csr_wen,
+  rf.io.wdata <> Mux(
+    wb_reg.out.wb_csr_ctrl.csr_wen,
     wb_reg.out.src2,
-    Mux(wb_reg.out.r_pmem,
-      wb_reg.out.mem_data,
-      wb_reg.out.alu_res))
+    Mux(wb_reg.out.r_pmem, wb_reg.out.mem_data, wb_reg.out.alu_res)
+  )
   rf.io.wb_rf_ctrl <> wb_reg.out.wb_rf_ctrl
 
   csr.io.wb_csr_ctrl <> wb_reg.out.wb_csr_ctrl
