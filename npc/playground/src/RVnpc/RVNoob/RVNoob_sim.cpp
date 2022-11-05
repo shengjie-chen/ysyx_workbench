d0a26f4de06b3e2f4d68651f739e1ee5d2800077
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-05 12:50:02 +0800

    >  sim RVNoob RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:50:02 up 19:13,  1 user,  load average: 0.59, 0.36, 0.36

--- npc/playground/src/RVnpc/RVNoob/RVNoob_sim.cpp
+++ npc/playground/src/RVnpc/RVNoob/RVNoob_sim.cpp
@@ -157,7 +157,7 @@ extern "C" void pmem_write_dpi(long long waddr, long long wdata, char wmask) {
 #ifdef CONFIG_MTRACE
   fprintf(mtrace_fp, "write pmem ## addr: %llx", waddr & ~0x7ull);
   fprintf(mtrace_fp, " -> 0x%016llx ", wdata);
-  fprintf(mtrace_fp, " wmask-> 0x%08x \n", wmask);
+  fprintf(mtrace_fp, " wmask-> 0x%02x \n", wmask);
 #endif
   for (int i = 0; i < 8; i++) {
     if ((wmask >> i) & 1 == 1) {
