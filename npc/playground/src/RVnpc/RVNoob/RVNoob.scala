bf279239e2cdf4ac64e96ca88bd77ca35b3c5bfc
tracer-ysyx2204 <tracer@ysyx.org>
2022-11-22 12:08:23 +0800

    >  sim RVNoob RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:08:23 up  7:02,  1 user,  load average: 0.65, 0.44, 0.41

--- npc/playground/src/RVnpc/RVNoob/RVNoob.scala
+++ npc/playground/src/RVnpc/RVNoob/RVNoob.scala
@@ -7,9 +7,9 @@ import RVnpc.RVNoob.Cache.ICache
 
 class RVNoob(pipeline: Boolean = true) extends Module with ext_function with RVNoobConfig {
   val io = IO(new Bundle {
-    val pc      = Output(UInt(64.W))
-    val inst    = Output(UInt(32.W))
-    val ebreak  = Output(Bool())
+    val pc = Output(UInt(64.W))
+    val inst = Output(UInt(32.W))
+    val ebreak = Output(Bool())
     val diff_en = Output(Bool())
     val diff_pc = Output(UInt(64.W))
     //    override val prefix
@@ -20,23 +20,23 @@ class RVNoob(pipeline: Boolean = true) extends Module with ext_function with RVN
   // ********************************** Instance **********************************
   // >>>>>>>>>>>>>> IF inst Fetch <<<<<<<<<<<<<<
   val dnpc_en = Wire(Bool())
-  val npc     = Wire(UInt(64.W))
+  val npc = Wire(UInt(64.W))
   dontTouch(npc)
-  val pc_en  = Wire(Bool())
-  val pc     = RegEnable(npc, 0x80000000L.U(64.W), pc_en) //2147483648
-  val snpc   = Wire(UInt(64.W))
+  val pc_en = Wire(Bool())
+  val pc = RegEnable(npc, 0x80000000L.U(64.W), pc_en) //2147483648
+  val snpc = Wire(UInt(64.W))
   val icache = Module(new ICache)
 
   // >>>>>>>>>>>>>> ID Inst Decode  id_reg <<<<<<<<<<<<<<
-  val ppl_ctrl   = Module(new PipelineCtrl)
-  val id_reg     = IDreg(pc, icache.io.inst_data, snpc, ppl_ctrl.io.id_reg_ctrl.en, pipelineBypass)
-  val idu        = Module(new IDU)
-  val rf         = Module(new RegisterFile)
-  val csr        = Module(new CSR)
+  val ppl_ctrl = Module(new PipelineCtrl)
+  val id_reg = IDreg(pc, icache.io.inst_data, snpc, ppl_ctrl.io.id_reg_ctrl.en, pipelineBypass)
+  val idu = Module(new IDU)
+  val rf = Module(new RegisterFile)
+  val csr = Module(new CSR)
   val cache_miss = Wire(Bool())
 
   // >>>>>>>>>>>>>> EXE ex_reg <<<<<<<<<<<<<<
-  val dnpc        = Wire(UInt(64.W))
+  val dnpc = Wire(UInt(64.W))
   val npc_add_res = Wire(UInt(64.W))
   val ex_reg: EXreg = EXreg(
     id_reg.out.pc,
@@ -54,7 +54,7 @@ class RVNoob(pipeline: Boolean = true) extends Module with ext_function with RVN
     ppl_ctrl.io.ex_reg_ctrl.en,
     pipelineBypass
   )
-  val exe      = Module(new EXE)
+  val exe = Module(new EXE)
   val exe_src1 = Wire(UInt(xlen.W))
   val exe_src2 = Wire(UInt(xlen.W))
 
@@ -73,7 +73,7 @@ class RVNoob(pipeline: Boolean = true) extends Module with ext_function with RVN
     ppl_ctrl.io.mem_reg_ctrl.en,
     pipelineBypass
   )
-  val datam      = Module(new DATAM)
+  val datam = Module(new DATAM)
   val judge_load = Module(new JudgeLoad)
 
   // >>>>>>>>>>>>>> WB wb_reg <<<<<<<<<<<<<<
@@ -90,34 +90,34 @@ class RVNoob(pipeline: Boolean = true) extends Module with ext_function with RVN
     pipelineBypass
   )
   val not_csr_wdata = Wire(UInt(xlen.W))
-  val U_ebreak      = Ebreak(clock, wb_reg.out.inst, ShiftRegister(rf.io.a0, 3, 1.B), io.ebreak)
+  val U_ebreak = Ebreak(clock, wb_reg.out.inst, ShiftRegister(rf.io.a0, 3, 1.B), io.ebreak)
 
   // ********************************** Connect and Logic **********************************
   // >>>>>>>>>>>>>> IF inst Fetch <<<<<<<<<<<<<<
-  snpc    := pc + 4.U
+  snpc := pc + 4.U
   dnpc_en := ex_reg.out.dnpc_ctrl.pc_mux || exe.io.B_en
-  pc_en   := ppl_ctrl.io.pc_en
-  npc     := Mux(dnpc_en, dnpc, snpc)
-  io.pc   := pc
+  pc_en := ppl_ctrl.io.pc_en
+  npc := Mux(dnpc_en, dnpc, snpc)
+  io.pc := pc
   val dpi_npc = Module(new DpiNpc) // use to get npc in sim.c
   dpi_npc.io.npc <> npc
-  io.inst        := icache.io.inst_data
+  io.inst := icache.io.inst_data
 
   icache.io.inst_addr <> pc
-  icache.io.inst_ren  <> !reset.asBool()
+  icache.io.inst_ren <> !reset.asBool()
 
   // >>>>>>>>>>>>>> ID Inst Decode  id_reg <<<<<<<<<<<<<<
   cache_miss := icache.io.miss
 
-  ppl_ctrl.io.idu_rf          <> idu.io.id_rf_ctrl
-  ppl_ctrl.io.idu_csr         <> idu.io.id_csr_ctrl
-  ppl_ctrl.io.ex_reg_rf       <> ex_reg.out.wb_rf_ctrl
-  ppl_ctrl.io.ex_reg_csr      <> ex_reg.out.wb_csr_ctrl
+  ppl_ctrl.io.idu_rf <> idu.io.id_rf_ctrl
+  ppl_ctrl.io.idu_csr <> idu.io.id_csr_ctrl
+  ppl_ctrl.io.ex_reg_rf <> ex_reg.out.wb_rf_ctrl
+  ppl_ctrl.io.ex_reg_csr <> ex_reg.out.wb_csr_ctrl
   ppl_ctrl.io.ex_reg_mem_ctrl <> ex_reg.out.mem_ctrl
-  ppl_ctrl.io.mem_reg_rf      <> mem_reg.out.wb_rf_ctrl
-  ppl_ctrl.io.mem_reg_csr     <> mem_reg.out.wb_csr_ctrl
-  ppl_ctrl.io.dnpc_en         <> dnpc_en
-  ppl_ctrl.io.miss            <> cache_miss
+  ppl_ctrl.io.mem_reg_rf <> mem_reg.out.wb_rf_ctrl
+  ppl_ctrl.io.mem_reg_csr <> mem_reg.out.wb_csr_ctrl
+  ppl_ctrl.io.dnpc_en <> dnpc_en
+  ppl_ctrl.io.miss <> cache_miss
 
   id_reg.reset <> ppl_ctrl.io.id_reg_ctrl.flush
 
@@ -157,23 +157,23 @@ class RVNoob(pipeline: Boolean = true) extends Module with ext_function with RVN
     exe.io.src1 <> ex_reg.out.src1
     exe.io.src2 <> ex_reg.out.src2
   }
-  exe.io.imm  <> ex_reg.out.imm
-  exe.io.pc   <> ex_reg.out.pc
+  exe.io.imm <> ex_reg.out.imm
+  exe.io.pc <> ex_reg.out.pc
   exe.io.snpc <> ex_reg.out.snpc
   exe.io.ctrl <> ex_reg.out.exe_ctrl
 
   // >>>>>>>>>>>>>> MEM mem_reg <<<<<<<<<<<<<<
   mem_reg.reset <> ppl_ctrl.io.mem_reg_ctrl.flush
 
-  datam.io.mem_ctrl  <> mem_reg.out.mem_ctrl
-  datam.io.wdata     <> mem_reg.out.src2
+  datam.io.mem_ctrl <> mem_reg.out.mem_ctrl
+  datam.io.wdata <> mem_reg.out.src2
   datam.io.data_addr <> mem_reg.out.mem_addr
 
   judge_load.io.judge_load_op <> mem_reg.out.mem_ctrl.judge_load_op
-  judge_load.io.mem_data      <> datam.io.rdata
+  judge_load.io.mem_data <> datam.io.rdata
 
   // >>>>>>>>>>>>>> WB wb_reg <<<<<<<<<<<<<<
-  wb_reg.reset  <> ppl_ctrl.io.wb_reg_ctrl.flush
+  wb_reg.reset <> ppl_ctrl.io.wb_reg_ctrl.flush
   not_csr_wdata := Mux(wb_reg.out.r_pmem, wb_reg.out.mem_data, wb_reg.out.alu_res)
   rf.io.wdata <> Mux(
     wb_reg.out.wb_csr_ctrl.csr_wen,
@@ -183,15 +183,18 @@ class RVNoob(pipeline: Boolean = true) extends Module with ext_function with RVN
   rf.io.wb_rf_ctrl <> wb_reg.out.wb_rf_ctrl
 
   csr.io.wb_csr_ctrl <> wb_reg.out.wb_csr_ctrl
-  csr.io.pc          <> wb_reg.out.pc
-  csr.io.mcause      <> 11.U
-  csr.io.csr_wdata   <> wb_reg.out.alu_res
+  csr.io.pc <> wb_reg.out.pc
+  csr.io.mcause <> 11.U
+  csr.io.csr_wdata <> wb_reg.out.alu_res
 
   // ********************************** Difftest **********************************
+  val cache_miss_last = !cache_miss && RegNext(cache_miss)
+  val cache_miss_first = cache_miss && !RegNext(cache_miss)
+
   if (pipeline) {
     // wb 写完成的周期
-    io.diff_en := ShiftRegister(wb_reg.in.reg_en, 2, 1.B) &&
-    (ShiftRegister(wb_reg.out.inst, 1, 1.B) =/= 0.U) && (!cache_miss || (cache_miss && RegNext(!cache_miss))) //
+    io.diff_en := (ShiftRegister(wb_reg.in.reg_en, 2, 1.B) || cache_miss_first) &&
+      (ShiftRegister(wb_reg.out.inst, 1, 1.B) =/= 0.U) && (!cache_miss || cache_miss_first) 
     when(wb_reg.out.pc =/= 0.U) {
       io.diff_pc := wb_reg.out.pc
     }.elsewhen(ShiftRegister(dnpc_en, 3, 1.B)) {
