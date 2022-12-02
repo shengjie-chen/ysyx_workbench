82b43d479c829310ae8ea21228aa9814d7c87427
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-02 15:24:43 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:24:43 up  2:54,  1 user,  load average: 0.30, 0.59, 0.60

--- npc/playground/src/RVnpc/RVNoob/RVNoob.scala
+++ npc/playground/src/RVnpc/RVNoob/RVNoob.scala
@@ -201,14 +201,16 @@ class RVNoob(pipeline: Boolean = true) extends Module with ext_function with RVN
 //    (ShiftRegister(wb_reg.out.inst, 1, 1.B) =/= 0.U) && (!cache_miss || cache_miss_first)
     io.diff_en := RegNext(wb_reg.out.valid)
     io.diff_pc := diff_pc
-    when(mem_reg.out.valid) {
-      io.diff_pc := mem_reg.out.pc
-    }.elsewhen(ex_reg.out.valid) {
-      io.diff_pc := ex_reg.out.pc
-    }.elsewhen(id_reg.out.valid) {
-      io.diff_pc := id_reg.out.pc
-    }.otherwise {
-      io.diff_pc := pc
+    when(wb_reg.out.valid) {
+      when(mem_reg.out.valid) {
+        io.diff_pc := mem_reg.out.pc
+      }.elsewhen(ex_reg.out.valid) {
+        io.diff_pc := ex_reg.out.pc
+      }.elsewhen(id_reg.out.valid) {
+        io.diff_pc := id_reg.out.pc
+      }.otherwise {
+        io.diff_pc := pc
+      }
     }
 
 //    when(wb_reg.out.pc =/= 0.U) {
