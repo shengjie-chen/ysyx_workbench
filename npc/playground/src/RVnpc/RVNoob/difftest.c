8d2cb901952b6f082be2e3156ad6bd21d125aa2b
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-08 20:58:39 +0800

    >  sim RVNoob RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:58:39 up 17:12,  1 user,  load average: 0.23, 0.31, 0.29

--- npc/playground/src/RVnpc/RVNoob/difftest.c
+++ npc/playground/src/RVnpc/RVNoob/difftest.c
@@ -2,7 +2,7 @@
  * @Author: Shengjie Chen chenshengjie1999@126.com
  * @Date: 2022-11-05 16:32:16
  * @LastEditors: Shengjie Chen chenshengjie1999@126.com
- * @LastEditTime: 2022-12-08 17:02:01
+ * @LastEditTime: 2022-12-08 20:58:12
  * @FilePath: /npc/playground/src/RVnpc/RVNoob/difftest.c
  * @Description: difftest相关的变量与函数
  */
@@ -21,6 +21,9 @@ char *diff_file = NULL;
 
 enum { DIFFTEST_TO_DUT,
        DIFFTEST_TO_REF };
+
+uint32_t ref_mem_temp;
+paddr_t ref_mem_temp_addr = 0x8001341c;
 
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
@@ -120,7 +123,7 @@ void isa_reg_display(CPU_state *ref) {
 /// @param ref
 /// @param pc 上一周期wb的pc，也就是当前检测pc
 void checkregs(CPU_state *ref, vaddr_t pc) {
-  if (!isa_difftest_checkregs(ref)) {
+  if (!isa_difftest_checkregs(ref) || (ref_mem_temp != 0)) {
     npc_state.state = NPC_ABORT;
     npc_state.halt_pc = pc;
     printf("diff test fail!!\n");
@@ -159,8 +162,12 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
         return;
       }
     }
+
     refresh_gpr_pc_csr();
     ref_difftest_exec(1);
+
+    ref_difftest_memcpy(ref_mem_temp_addr, &ref_mem_temp, 4, DIFFTEST_TO_DUT);
+
     ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
     checkregs(&ref_r, wb_pc);
 
