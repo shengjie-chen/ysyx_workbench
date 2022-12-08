2d3df1ad1e04afadc330f973459a9794162ed9ac
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-08 10:43:33 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:43:33 up 11:14,  1 user,  load average: 0.27, 0.29, 0.32

--- npc/playground/src/RVnpc/RVNoob/difftest.c
+++ npc/playground/src/RVnpc/RVNoob/difftest.c
@@ -2,7 +2,7 @@
  * @Author: Shengjie Chen chenshengjie1999@126.com
  * @Date: 2022-11-05 16:32:16
  * @LastEditors: Shengjie Chen chenshengjie1999@126.com
- * @LastEditTime: 2022-12-08 09:51:05
+ * @LastEditTime: 2022-12-08 10:43:01
  * @FilePath: /npc/playground/src/RVnpc/RVNoob/difftest.c
  * @Description: difftest相关的变量与函数
  */
@@ -27,17 +27,13 @@ void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
-/// @brief 从硬件那里更新仿真软件这边的cpu寄存器
+/// @brief 从硬件那里更新仿真软件这边的cpu寄存器。
 void refresh_gpr_pc_csr() {
   int i;
   for (i = 0; i < 32; i++) {
     cpu_state.gpr[i] = cpu_gpr[i];
   }
-#ifdef CONFIG_PIPELINE
-  cpu_state.pc = top->io_diff_pc;
-#else
-  cpu_state.pc = top->io_pc;
-#endif
+  cpu_state.pc = top->io_diff_pc; 
   for (i = 0; i < CSR_NUM; i++) {
     cpu_state.csr[i] = cpu_csr[i];
   }
@@ -120,6 +116,9 @@ void isa_reg_display(CPU_state *ref) {
   }
 }
 
+/// @brief 
+/// @param ref 
+/// @param pc 上一周期wb的pc，也就是当前检测pc
 void checkregs(CPU_state *ref, vaddr_t pc) {
   if (!isa_difftest_checkregs(ref)) {
     npc_state.state = NPC_ABORT;
@@ -130,15 +129,10 @@ void checkregs(CPU_state *ref, vaddr_t pc) {
   }
 }
 
-#ifdef CONFIG_PIPELINE
 static int is_skip_ref_num = 0;
 vaddr_t wb_pc = 0x80000000;
-#else
-static bool is_skip_ref = false;
-#endif
 
 void difftest_step(vaddr_t pc, vaddr_t npc) {
-#ifdef CONFIG_PIPELINE
   CPU_state ref_r;
   if (top->io_diff_en) {
     if (is_skip_ref_num != 0) {
@@ -156,45 +150,10 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
     checkregs(&ref_r, wb_pc);
     wb_pc = top->io_diff_pc;
   }
-
-#else
-  CPU_state ref_r;
-
-  // if (skip_dut_nr_inst > 0) {
-  //   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
-  //   if (ref_r.pc == npc) {
-  //     skip_dut_nr_inst = 0;
-  //     checkregs(&ref_r, npc);
-  //     return;
-  //   }
-  //   skip_dut_nr_inst--;
-  //   if (skip_dut_nr_inst == 0)
-  //     panic("can not catch up with ref.pc = " FMT_WORD " at pc = " FMT_WORD, ref_r.pc, pc);
-  //   return;
-  // }
-
-  if (is_skip_ref) {
-    // to skip the checking of an instruction, just copy the reg state to reference design
-    refresh_gpr_pc_csr();                             // 更新状态
-    ref_difftest_regcpy(&cpu_state, DIFFTEST_TO_REF); // 把状态拷贝到nemu
-    is_skip_ref = false;
-    return;
-  }
-
-  ref_difftest_exec(1);
-  ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
-
-  refresh_gpr_pc_csr();
-  checkregs(&ref_r, pc);
-#endif
 }
 
 /// @brief 对外设进行读写时需要跳过difftest
 /// @todo 因为读写发生在mem阶段，需要记录pc或者inst，等到这个指令执行到wb阶段，再进行跳过
 void difftest_skip_ref() {
-#ifdef CONFIG_PIPELINE
   is_skip_ref_num++;
-#else
-  is_skip_ref = true;
-#endif
 }
