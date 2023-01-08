31fc49585fdd3558b8986f48d5b6fdee984e1abb
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-08 21:28:35 +0800

    >  sim RVNoob RTL ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:28:35 up  7:07,  1 user,  load average: 0.68, 0.44, 0.40

--- npc/playground/src/RVnpc/RVNoob/conf.h
+++ npc/playground/src/RVnpc/RVNoob/conf.h
@@ -15,12 +15,12 @@
 // #define CONFIG_FTRACE
 // #define CONFIG_MTRACE
 
-#define CONFIG_DIFFTEST
+// #define CONFIG_DIFFTEST
 // #define CONFIG_DIFFTEST_REF_MEM_POINT
 
 // 1M --> 1.6GB
-#define CONFIG_DUMPVCD
-#define CONFIG_DUMPSTART 0
+// #define CONFIG_DUMPVCD
+// #define CONFIG_DUMPSTART 0
 // #define CONFIG_DUMPSTART 1200000
 
 // ---------------------------->switch end
