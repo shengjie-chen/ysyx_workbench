858584732165b1a4419e8c2f28fcd96ab20fb757
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-17 13:10:58 +0800

    >  generate RVNoob verilog ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:10:58 up  4:19,  1 user,  load average: 1.77, 1.30, 1.05

--- npc/playground/src/RVnpc/RVNoob/CSR.scala
+++ npc/playground/src/RVnpc/RVNoob/CSR.scala
@@ -40,6 +40,10 @@ class CSR extends Module with RVNoobConfig with Csr_op {
     DpiCsrRead.io.csr(3) <> mcause
   }
 
+  read_mstatus := mstatus
+  read_mtvec   := mtvec
+  read_mepc    := mepc
+  read_mcause  := mcause
   when(io.wb_csr_ctrl.csr_wen) {
     switch(io.wb_csr_ctrl.csr_waddr) {
       is(0x300.U) {
@@ -69,11 +73,6 @@ class CSR extends Module with RVNoobConfig with Csr_op {
   }.elsewhen(io.wb_csr_ctrl.mret) {
     mstatus      := read_mstatus
     read_mstatus := mstatus(xlen - 1, 4) ## mstatus(7) ## mstatus(2, 0)
-  }.otherwise {
-    read_mstatus := mstatus
-    read_mtvec   := mtvec
-    read_mepc    := mepc
-    read_mcause  := mcause
   }
 
   val csr_read = MuxCase(
