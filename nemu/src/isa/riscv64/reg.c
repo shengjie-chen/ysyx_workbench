adea1a0eed246874c66bbfd32a564c445d59aff7
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-10 14:21:55 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:21:55 up  9:58,  5 users,  load average: 0.60, 0.36, 0.24

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -22,6 +22,7 @@ word_t isa_reg_str2val(const char *s, bool *success)
   char *reg = 0;
   if (*(s + 1) > 48 && *(s + 1) < 57) {
     sscanf(s + 1, "%d", &reg_index);
+    printf("%d\n",reg_index);
     if (reg_index >= 32) {
       *success = false;
       panic("reg index overflow\n");
