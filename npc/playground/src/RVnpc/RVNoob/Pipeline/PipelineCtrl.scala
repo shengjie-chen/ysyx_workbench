88ae25933e62c1a3c4976aeb336ef1b7929d6076
Shengjie Chen <chenshengjie1999@126.com>
2023-03-15 19:48:03 +0800

    删除csr数据冒险的无效条件

--- npc/playground/src/RVnpc/RVNoob/Pipeline/PipelineCtrl.scala
+++ npc/playground/src/RVnpc/RVNoob/Pipeline/PipelineCtrl.scala
@@ -46,9 +46,9 @@ class PipelineCtrl extends Module with RVNoobConfig {
     (io.idu_rf.rs2 === io.mem_reg_rf.rd) && (io.mem_reg_rf.rd =/= 0.U) && io.mem_reg_rf.wen && io.idu_rf.ren2
 
   val ex_csr_hazard =
-    (io.ex_reg_csr.csr_wen && io.idu_csr.csr_ren && io.idu_csr.csr_raddr === io.ex_reg_csr.csr_waddr) || (io.ex_reg_csr.ecall && io.idu_csr.csr_raddr === 0x341.U) // &&csr_addr === mcause
+    io.ex_reg_csr.csr_wen && io.idu_csr.csr_ren && io.idu_csr.csr_raddr === io.ex_reg_csr.csr_waddr
   val mem_csr_hazard =
-    (io.mem_reg_csr.csr_wen && io.idu_csr.csr_ren && io.idu_csr.csr_raddr === io.mem_reg_csr.csr_waddr) || (io.mem_reg_csr.ecall && io.idu_csr.csr_raddr === 0x341.U) // &&csr_addr === mcause
+    io.mem_reg_csr.csr_wen && io.idu_csr.csr_ren && io.idu_csr.csr_raddr === io.mem_reg_csr.csr_waddr
 
   // Forward
   val forward1 =
