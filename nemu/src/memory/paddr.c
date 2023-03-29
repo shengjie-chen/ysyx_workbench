f7e29bb39eb9cbc4ef1778e95d928ed9c95dd5bd
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-29 11:22:53 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:22:53 up  5:46,  1 user,  load average: 0.11, 0.49, 0.52

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -105,7 +105,8 @@ word_t paddr_read(paddr_t addr, int len) {
   //     difftest_skip_ref();
   //   }
   // #endif
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+
+  IFDEF(CONFIG_DEVICE, difftest_skip_ref(); return mmio_read(addr, len));
 #ifdef CONFIG_MTRACE
   fprintf(mtrace_fp, " -> addr is out of bound!\n");
 #endif
@@ -119,7 +120,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
     pmem_write(addr, len, data);
     return;
   }
-  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return );
+  IFDEF(CONFIG_DEVICE, difftest_skip_ref(); mmio_write(addr, len, data); return);
 #ifdef CONFIG_MTRACE
   fprintf(mtrace_fp, " -> addr is out of bound!\n");
 #endif
