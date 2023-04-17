8b0985a52bda9201f907ba996a21e9c99dc2aa5f
Shengjie Chen <chenshengjie1999@126.com>
2023-04-17 21:07:51 +0800

    add am-kernels

---
+++ am-kernels/tests/am-tests/src/tests/intr.c
@@ -0,0 +1,29 @@
+#include <amtest.h>
+#define CLINT_MTIME 0x200bff8
+#define CLINT_MTIMECMP 0x2004000
+
+Context *simple_trap(Event ev, Context *ctx) {
+  switch(ev.event) {
+    case EVENT_IRQ_TIMER:
+      *((uint64_t *)CLINT_MTIMECMP) += 700000;
+      putch('t'); putch('\n'); break;
+    case EVENT_IRQ_IODEV:
+      putch('d'); break;
+    case EVENT_YIELD:
+      putch('y'); break;
+    default:
+      break;
+  }
+  return ctx;
+}
+
+void hello_intr() {
+  printf("Hello, AM World @ " __ISA__ "\n");
+  printf("  t = timer, d = device, y = yield\n");
+  io_read(AM_INPUT_CONFIG);
+  iset(1);
+  while (1) {
+    for (volatile int i = 0; i < 10000000; i++) ;
+    yield();
+  }
+}
