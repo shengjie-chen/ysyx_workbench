8acc990922f3c0c09f5dfafde5a84db038b16a33
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-28 23:18:08 +0800

    >  compile NEMU ysyx_22040495 陈昇杰 Linux jiexxpu-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:18:08 up 18:50,  7 users,  load average: 0.15, 0.34, 0.35

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -183,7 +183,7 @@ void init_log(const char *log_file) {
   Log("Mem Trace Log is written to %s", mtrace_file);
   realpath_bool = realpath(mtrace_file, resolved_path);
   printf("1\n");
-  if (realpath_bool != NULL) {
+  if (realpath_bool == NULL) {
   printf("1\n");
 
     Log("Mem Trace Log is written to %s", resolved_path);
