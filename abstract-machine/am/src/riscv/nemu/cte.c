6c9b7c5f07ecee09e6eab5e09ade8ec12a354b42
Shengjie Chen <chenshengjie1999@126.com>
2024-04-19 09:23:34 +0800

    run yield-os

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -42,7 +42,12 @@ bool cte_init(Context *(*handler)(Event, Context *)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+    Context *c = (Context *)(kstack.end - sizeof(Context));
+	memset(c, 0, sizeof(Context));
+	c->mstatus = 0xa00001800;
+	c->mepc = (uintptr_t)entry;
+	c->gpr[10] = (uint64_t)arg;
+    return c;
 }
 
 void yield() {
